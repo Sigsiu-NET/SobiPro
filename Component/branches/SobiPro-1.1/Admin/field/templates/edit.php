6817e8eea56634686f5bf7bcb0938ac234761726
Radek Suski <Radek Suski@42bd815b-6740-4516-9028-ea9f7a097aa0>
2012-08-10 16:43:24 +0000

    1.1 - new tpl

---
+++ Component/branches/SobiPro-1.1/Admin/field/templates/edit.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * @version: $Id: inbox.php 551 2011-01-11 14:34:26Z Radek Suski $
+ * @package: SobiPro Template
+ * ===================================================
+ * @author
+ * Name: Sigrid Suski & Radek Suski, Sigsiu.NET GmbH
+ * Email: sobi[at]sigsiu.net
+ * Url: http://www.Sigsiu.NET
+ * ===================================================
+ * @copyright Copyright (C) 2006 - 2011 Sigsiu.NET GmbH (http://www.sigsiu.net). All rights reserved.
+ * @license see http://www.gnu.org/licenses/gpl.html GNU/GPL Version 3.
+ * You can use, redistribute this file and/or modify it under the terms of the GNU General Public License version 3
+ * ===================================================
+ * $Date: 2011-01-11 15:34:26 +0100 (Tue, 11 Jan 2011) $
+ * $Revision: 551 $
+ * $Author: Radek Suski $
+ * $HeadURL: https://svn.suski.eu/SobiPro/Component/trunk/Admin/field/edit/inbox.php $
+ */
+defined( 'SOBIPRO' ) || exit( 'Restricted access' );
+?>
+<h1>1111</h1>
+
+<ul class="nav nav-tabs" id="myTab">
+	<li class="active"><a href="#home">Home</a></li>
+	<li><a href="#profile">Profile</a></li>
+	<li><a href="#messages">Messages</a></li>
+	<li><a href="#settings">Settings</a></li>
+</ul>
+
+<div class="tab-content">
+	<div class="tab-pane active" id="home">
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent nulla urna, molestie ut adipiscing
+		vulputate, elementum eu nunc. Suspendisse eget sapien nec diam suscipit rutrum at id tellus. Nullam auctor
+		pellentesque ipsum, sit amet suscipit mi dapibus et. Donec justo nibh, consectetur sit amet porttitor
+		dictum, gravida feugiat ipsum. Phasellus egestas vulputate velit ac semper. Suspendisse accumsan ipsum ac
+		diam blandit placerat. Pellentesque pulvinar blandit vehicula. Duis ac massa id mauris elementum vulputate.
+		Quisque dui orci, imperdiet vel convallis id, aliquet quis magna. Suspendisse potenti. Curabitur viverra
+		interdum sapien, non vehicula dui lacinia pretium. Vivamus malesuada viverra leo, molestie tincidunt tortor
+		posuere id. Phasellus ornare sapien et dui convallis eget fringilla purus venenatis.
+	</div>
+	<div class="tab-pane" id="profile">
+		Vestibulum eu dui tortor. Nunc porttitor pharetra eleifend. Vestibulum lobortis dapibus dignissim. Cras sed
+		odio in risus pharetra ornare. Morbi aliquam venenatis metus, a ultricies erat malesuada id. Quisque a quam
+		ligula, eu suscipit leo. Nulla blandit auctor nunc, eget pretium enim consectetur vitae. Aenean et ultricies
+		ligula. Sed sed nisi nec dui semper sagittis. Quisque interdum mollis justo ut pretium. Proin cursus
+		ultrices nunc, eu molestie tellus adipiscing et. Curabitur velit ligula, pellentesque at ullamcorper ac,
+		imperdiet adipiscing quam. Ut tortor velit, mollis ut malesuada non, varius scelerisque neque.
+	</div>
+	<div class="tab-pane" id="messages">
+		$HeadURL: https://svn.suski.eu/SobiPro/Component/trunk/Admin/field/edit/inbox.php $
+	</div>
+	<div class="tab-pane" id="settings">
+		use, redistribute this file and/or modify it under the terms of the GNU General Public License version 3
+		* ===================================================
+	</div>
+</div>
