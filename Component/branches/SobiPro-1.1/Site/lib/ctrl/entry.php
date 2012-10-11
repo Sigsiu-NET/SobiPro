db5372d0075f9f85a70d39ab6af1d2283951007a
Sigrid Suski <SigsiuTrinity@users.noreply.github.com>
2012-10-11 09:19:35 +0000

    (#) Typo in text label in Entry Manager

--- Component/branches/SobiPro-1.1/Site/lib/ctrl/entry.php
+++ Component/branches/SobiPro-1.1/Site/lib/ctrl/entry.php
@@ -103,7 +103,7 @@ class SPEntryCtrl extends SPController
     private function approve()
     {
         if ( $this->_model->isCheckedOut() ) {
-            Sobi::Redirect( Sobi::Back(), Sobi::Txt( 'EN.IS_CHECEKD_OUT' ), SPC::ERROR_MSG, true );
+            Sobi::Redirect( Sobi::Back(), Sobi::Txt( 'EN.IS_CHECKED_OUT' ), SPC::ERROR_MSG, true );
         }
         if ( ( ( $this->_model->get( 'owner' ) == Sobi::My( 'id' ) ) && Sobi::Can( 'entry.manage.own' ) ) || Sobi::Can( 'entry.manage.*' ) ) {
             try {
