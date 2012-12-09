e2510f98aa79d3f1ef1783a55d75cd3ce721b344
Radek Suski <suski.radek@gmail.com>
2012-12-09 10:33:36 +0000

    legacy mode - removing old styles with jQuery

---
+++ Component/branches/SobiPro-1.1/Site/lib/js/adm/legacy.js
@@ -0,0 +1,13 @@
+/**
+ * Created with JetBrains PhpStorm.
+ * User: neo
+ * Date: 9/12/2012
+ * Time: 11:19
+ * To change this template use File | Settings | File Templates.
+ */
+SobiPro.jQuery( document ).ready( function ()
+{
+	"use strict";
+	SobiPro.jQuery( '#SPAdminForm >:first-child' ).removeAttr( 'style' ).addClass( 'span2' );
+	SobiPro.jQuery( '#SPAdminForm >:nth-child(2)' ).removeAttr( 'style' ).addClass( 'span10' );
+} );
