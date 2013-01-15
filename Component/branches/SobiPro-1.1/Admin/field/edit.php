cc8719a4cbfc09a591cff135b92723a4916b0011
Sigrid Suski <Sigrid Suski@42bd815b-6740-4516-9028-ea9f7a097aa0>
2013-01-15 09:51:50 +0000

    propset, new header set

--- Component/branches/SobiPro-1.1/Admin/field/edit.php
+++ Component/branches/SobiPro-1.1/Admin/field/edit.php
@@ -1,22 +1,26 @@
 <?php
 /**
  * @version: $Id$
- * @package: SobiPro Template
- * ===================================================
+ * @package: SobiPro Component for Joomla!
+
  * @author
  * Name: Sigrid Suski & Radek Suski, Sigsiu.NET GmbH
  * Email: sobi[at]sigsiu.net
  * Url: http://www.Sigsiu.NET
- * ===================================================
- * @copyright Copyright (C) 2006 - 2012 Sigsiu.NET GmbH (http://www.sigsiu.net). All rights reserved.
- * @license see http://www.gnu.org/licenses/gpl.html GNU/GPL Version 3.
- * You can use, redistribute this file and/or modify it under the terms of the GNU General Public License version 3
- * ===================================================
+
+ * @copyright Copyright (C) 2006 - 2013 Sigsiu.NET GmbH (http://www.sigsiu.net). All rights reserved.
+ * @license GNU/GPL Version 3
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License version 3 as published by the Free Software Foundation, and under the additional terms according section 7 of GPL v3.
+ * See http://www.gnu.org/licenses/gpl.html and http://sobipro.sigsiu.net/licenses.
+
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
  * $Date$
  * $Revision$
  * $Author$
  * $HeadURL$
  */
+
 defined( 'SOBIPRO' ) || exit( 'Restricted access' );
 SPRequest::set( 'hidemainmenu', 1 );
 SPFactory::header()->addCSSCode( 'body { min-width: 1200px; }' );
--- Component/branches/SobiPro-1.1/Admin/sobipro.php
+++ Component/branches/SobiPro-1.1/Admin/sobipro.php
@@ -2,21 +2,25 @@
 /**
  * @version: $Id$
  * @package: SobiPro Component for Joomla!
- * ===================================================
+
  * @author
  * Name: Sigrid Suski & Radek Suski, Sigsiu.NET GmbH
  * Email: sobi[at]sigsiu.net
  * Url: http://www.Sigsiu.NET
- * ===================================================
- * @copyright Copyright (C) 2006 - 2012 Sigsiu.NET GmbH (http://www.sigsiu.net). All rights reserved.
- * @license see http://www.gnu.org/licenses/gpl.html GNU/GPL Version 3.
- * You can use, redistribute this file and/or modify it under the terms of the GNU General Public License version 3
- * ===================================================
+
+ * @copyright Copyright (C) 2006 - 2013 Sigsiu.NET GmbH (http://www.sigsiu.net). All rights reserved.
+ * @license GNU/GPL Version 3
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License version 3 as published by the Free Software Foundation, and under the additional terms according section 7 of GPL v3.
+ * See http://www.gnu.org/licenses/gpl.html and http://sobipro.sigsiu.net/licenses.
+
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
  * $Date$
  * $Revision$
  * $Author$
  * $HeadURL$
  */
+
 defined( '_JEXEC' ) || exit( 'Restricted access' );
 define( 'SOBI_TESTS', false );
 defined( 'DS' ) || define( 'DS', DIRECTORY_SEPARATOR );
