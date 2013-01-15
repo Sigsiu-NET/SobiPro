c0d0f84cb7e8f68d38fd01637d37e76b9e80b91c
Sigrid Suski <SigsiuTrinity@users.noreply.github.com>
2013-01-15 10:01:11 +0000

    propset, new header set

--- Component/branches/SobiPro-1.1/install.php
+++ Component/branches/SobiPro-1.1/install.php
@@ -1,21 +1,24 @@
 <?php
 /**
- * @version: $Id: install.php 2010 2011-11-24 10:07:38Z Radek Suski $
+ * @version: $Id$
  * @package: SobiPro Component for Joomla!
- * ===================================================
+
  * @author
  * Name: Sigrid Suski & Radek Suski, Sigsiu.NET GmbH
  * Email: sobi[at]sigsiu.net
  * Url: http://www.Sigsiu.NET
- * ===================================================
- * @copyright Copyright (C) 2006 - 2011 Sigsiu.NET GmbH (http://www.sigsiu.net). All rights reserved.
- * @license see http://www.gnu.org/licenses/gpl.html GNU/GPL Version 3.
- * You can use, redistribute this file and/or modify it under the terms of the GNU General Public License version 3
- * ===================================================
- * $Date: 2011-11-24 11:07:38 +0100 (Thu, 24 Nov 2011) $
- * $Revision: 2010 $
- * $Author: Radek Suski $
- * $HeadURL: https://svn.suski.eu/SobiPro/Component/trunk/install.php $
+
+ * @copyright Copyright (C) 2006 - 2013 Sigsiu.NET GmbH (http://www.sigsiu.net). All rights reserved.
+ * @license GNU/GPL Version 3
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License version 3 as published by the Free Software Foundation, and under the additional terms according section 7 of GPL v3.
+ * See http://www.gnu.org/licenses/gpl.html and http://sobipro.sigsiu.net/licenses.
+
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+ * $Date$
+ * $Revision$
+ * $Author$
+ * $HeadURL$
  */
 
 defined( '_JEXEC' ) || exit( 'Restricted access' );
