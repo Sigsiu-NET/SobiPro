3daf29893eaec5c4d10d78d2db1fa0ddae89dcd2
Sigrid Suski <SigsiuTrinity@users.noreply.github.com>
2013-01-15 12:21:38 +0000

    propset, new header set .htaccess files removed

--- Component/branches/SobiPro-1.1/Site/opt/fields/adm/email.php
+++ Component/branches/SobiPro-1.1/Site/opt/fields/adm/email.php
@@ -1,22 +1,26 @@
 <?php
 /**
- * @version: $Id: email.php 551 2011-01-11 14:34:26Z Radek Suski $
- * @package: SobiPro Library
- * ===================================================
+ * @version: $Id$
+ * @package: SobiPro Component for Joomla!
+
  * @author
  * Name: Sigrid Suski & Radek Suski, Sigsiu.NET GmbH
  * Email: sobi[at]sigsiu.net
  * Url: http://www.Sigsiu.NET
- * ===================================================
- * @copyright Copyright (C) 2006 - 2011 Sigsiu.NET GmbH (http://www.sigsiu.net). All rights reserved.
- * @license see http://www.gnu.org/licenses/lgpl.html GNU/LGPL Version 3.
- * You can use, redistribute this file and/or modify it under the terms of the GNU Lesser General Public License version 3
- * ===================================================
- * $Date: 2011-01-11 15:34:26 +0100 (Tue, 11 Jan 2011) $
- * $Revision: 551 $
- * $Author: Radek Suski $
- * $HeadURL: https://svn.suski.eu/SobiPro/Component/trunk/Site/opt/fields/adm/email.php $
+
+ * @copyright Copyright (C) 2006 - 2013 Sigsiu.NET GmbH (http://www.sigsiu.net). All rights reserved.
+ * @license GNU/GPL Version 3
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License version 3 as published by the Free Software Foundation, and under the additional terms according section 7 of GPL v3.
+ * See http://www.gnu.org/licenses/gpl.html and http://sobipro.sigsiu.net/licenses.
+
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+ * $Date$
+ * $Revision$
+ * $Author$
+ * $HeadURL$
  */
+
 defined( 'SOBIPRO' ) || exit( 'Restricted access' );
 SPLoader::loadClass( 'opt.fields.email' );
 
