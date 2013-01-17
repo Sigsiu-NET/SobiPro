b077ff7c351f4eab962224982ffd78d347763f01
Radek Suski <suski.radek@gmail.com>
2013-01-17 10:50:15 +0000

    legacy fixes

--- Component/branches/SobiPro-1.1/Site/opt/fields/category.php
+++ Component/branches/SobiPro-1.1/Site/opt/fields/category.php
@@ -2,19 +2,15 @@
 /**
  * @version: $Id$
  * @package: SobiPro Component for Joomla!
-
  * @author
  * Name: Sigrid Suski & Radek Suski, Sigsiu.NET GmbH
  * Email: sobi[at]sigsiu.net
  * Url: http://www.Sigsiu.NET
-
  * @copyright Copyright (C) 2006 - 2013 Sigsiu.NET GmbH (http://www.sigsiu.net). All rights reserved.
  * @license GNU/GPL Version 3
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License version 3 as published by the Free Software Foundation, and under the additional terms according section 7 of GPL v3.
  * See http://www.gnu.org/licenses/gpl.html and http://sobipro.sigsiu.net/licenses.
-
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
-
  * $Date$
  * $Revision$
  * $Author$
@@ -117,7 +113,7 @@ class SPField_Category extends SPFieldType implements SPFieldInterface
 		$delBtParams = array( 'class' => 'btn' );
 		$selectParams = array();
 		SPFactory::header()
-				->addJsFile( 'category-edit' )
+				->addJsFile( 'opt.field_category_tree' )
 				->addJsCode( 'SobiPro.jQuery( document ).ready( function () { new SigsiuTreeEdit( ' . json_encode( $params ) . '); } );' );
 		$selector .= '<div class="tree">' . $tree->display( true ) . '</div>';
 		if ( count( $this->_selectedCats ) ) {
@@ -137,7 +133,7 @@ class SPField_Category extends SPFieldType implements SPFieldInterface
 			$addBtParams[ 'disabled' ] = 'disabled';
 			$selectParams[ 'readonly' ] = 'readonly';
 		}
-		else {
+		elseif ( !( count( $selectedCategories ) ) ) {
 			$delBtParams[ 'disabled' ] = 'disabled';
 		}
 		$selector .= '<div class="selected">';
