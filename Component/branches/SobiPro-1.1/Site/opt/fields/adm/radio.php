43637f8c3ef2e051d2724d8843f9b651b4f35cae
Radek Suski <suski.radek@gmail.com>
2013-01-23 17:37:55 +0000

    übergeordneten .htaccess schreiben

--- Component/branches/SobiPro-1.1/Site/opt/fields/adm/radio.php
+++ Component/branches/SobiPro-1.1/Site/opt/fields/adm/radio.php
@@ -2,19 +2,15 @@
 /**
  * @version: $Id$
  * @package: SobiPro Component for Joomla!
-
  * @author
  * Name: Sigrid Suski & Radek Suski, Sigsiu.NET GmbH
  * Email: sobi[at]sigsiu.net
  * Url: http://www.Sigsiu.NET
-
  * @copyright Copyright (C) 2006 - 2013 Sigsiu.NET GmbH (http://www.sigsiu.net). All rights reserved.
  * @license GNU/GPL Version 3
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License version 3 as published by the Free Software Foundation, and under the additional terms according section 7 of GPL v3.
  * See http://www.gnu.org/licenses/gpl.html and http://sobipro.sigsiu.net/licenses.
-
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
-
  * $Date$
  * $Revision$
  * $Author$
@@ -40,21 +36,30 @@ class SPField_RadioAdm extends SPField_Radio
 	{
 		static $lang = null;
 		static $defLang = null;
-		if( !( $lang ) ) {
+		if ( !( $lang ) ) {
 			$lang = Sobi::Lang();
 			$defLang = Sobi::DefLang();
 		}
-		$options = array();
-		if( count( $attr['options'] ) ) {
+		$file = SPRequest::file( 'spfieldsopts', 'tmp_name' );
+		if ( $file ) {
+			$options = $this->parseOptsFile( parse_ini_file( $file, true ) );
+		}
+		else {
+			$options = $this->parseOptsFile( parse_ini_string( $attr[ 'options' ], true ) );
+		}
+		if ( count( $options ) ) {
+			unset( $attr[ 'options' ] );
+		}
+		if ( count( $attr[ 'options' ] ) ) {
 			$p = 0;
-			foreach ( $attr['options'] as $o ) {
-				if( isset( $o[ 'id' ] ) ) {
-					$options[] = array( 'id' => $o[ 'id' ], 'name' => $o[ 'name' ], 'parent' => null, 'position' => ++$p );
+			foreach ( $attr[ 'options' ] as $o ) {
+				if ( isset( $o[ 'id' ] ) ) {
+					$options[ ] = array( 'id' => $o[ 'id' ], 'name' => $o[ 'name' ], 'parent' => null, 'position' => ++$p );
 				}
 			}
-			unset( $attr['options'] );
+			unset( $attr[ 'options' ] );
 		}
-		if( count( $options ) ) {
+		if ( count( $options ) ) {
 			$optionsArr = array();
 			$labelsArr = array();
 			$optsIds = array();
@@ -62,18 +67,18 @@ class SPField_RadioAdm extends SPField_Radio
 			foreach ( $options as $i => $option ) {
 				/* check for doubles */
 				foreach ( $options as $pos => $opt ) {
-					if( $i == $pos ) {
+					if ( $i == $pos ) {
 						continue;
 					}
-					if( $option[ 'id' ] == $opt[ 'id' ] ) {
-						$option[ 'id' ] = $option[ 'id' ].'_'.substr( ( string ) microtime(), 2, 8 ).rand( 1, 100 );
+					if ( $option[ 'id' ] == $opt[ 'id' ] ) {
+						$option[ 'id' ] = $option[ 'id' ] . '_' . substr( ( string )microtime(), 2, 8 ) . rand( 1, 100 );
 						SPFactory::message()->warning( 'FIELD_WARN_DUPLICATE_OPT_ID' );
 					}
 				}
-				$optionsArr[] = array( 'fid' => $this->id, 'optValue' => $option[ 'id' ], 'optPos'  => $option[ 'position' ], 'optParent' =>  $option[ 'parent' ] );
-				$defLabelsArr[] = array( 'sKey' => $option[ 'id' ], 'sValue' => $option[ 'name' ], 'language' => $defLang, 'oType' => 'field_option', 'fid' => $this->id );
-				$labelsArr[] = array( 'sKey' => $option[ 'id' ], 'sValue' => $option[ 'name' ], 'language' => $lang, 'oType' => 'field_option', 'fid' => $this->id );
-				$optsIds[] = $option[ 'id' ];
+				$optionsArr[ ] = array( 'fid' => $this->id, 'optValue' => $option[ 'id' ], 'optPos' => $option[ 'position' ], 'optParent' => $option[ 'parent' ] );
+				$defLabelsArr[ ] = array( 'sKey' => $option[ 'id' ], 'sValue' => $option[ 'name' ], 'language' => $defLang, 'oType' => 'field_option', 'fid' => $this->id );
+				$labelsArr[ ] = array( 'sKey' => $option[ 'id' ], 'sValue' => $option[ 'name' ], 'language' => $lang, 'oType' => 'field_option', 'fid' => $this->id );
+				$optsIds[ ] = $option[ 'id' ];
 			}
 			/* @var SPdb $db */
 			$db =& SPFactory::db();
@@ -82,28 +87,26 @@ class SPField_RadioAdm extends SPField_Radio
 			try {
 				$db->delete( 'spdb_field_option', array( 'fid' => $this->id ) );
 				$db->delete( 'spdb_language', array( 'oType' => 'field_option', 'fid' => $this->id, '!sKey' => $optsIds ) );
-			}
-			catch ( SPException $x ) {
+			} catch ( SPException $x ) {
 				Sobi::Error( $this->name(), SPLang::e( 'CANNOT_DELETE_SELECTED_OPTIONS', $x->getMessage() ), SPC::ERROR, 500, __LINE__, __FILE__ );
 			}
 			/* insert new values */
 			try {
 				$db->insertArray( 'spdb_field_option', $optionsArr );
 				$db->insertArray( 'spdb_language', $labelsArr, true );
-				if( $defLang != $lang ) {
+				if ( $defLang != $lang ) {
 					$db->insertArray( 'spdb_language', $defLabelsArr, false, true );
 				}
-			}
-			catch ( SPException $x ) {
+			} catch ( SPException $x ) {
 				Sobi::Error( $this->name(), SPLang::e( 'CANNOT_STORE_FIELD_OPTIONS_DB_ERR', $x->getMessage() ), SPC::ERROR, 500, __LINE__, __FILE__ );
 			}
 		}
-		if( !isset( $attr[ 'params' ] ) ) {
+		if ( !isset( $attr[ 'params' ] ) ) {
 			$attr[ 'params' ] = array();
 		}
 		$myAttr = $this->getAttr();
 		$properties = array();
-		if( count( $myAttr ) ) {
+		if ( count( $myAttr ) ) {
 			foreach ( $myAttr as $property ) {
 				$properties[ $property ] = isset( $attr[ $property ] ) ? ( $attr[ $property ] ) : null;
 			}
