4bd0113171ba6eee172e3f4cc693f8514dc2f538
Radek Suski <suski.radek@gmail.com>
2013-01-27 17:15:19 +0000

    legacy mode for listings

--- Component/branches/SobiPro-1.1/Site/lib/ctrl/listing.php
+++ Component/branches/SobiPro-1.1/Site/lib/ctrl/listing.php
@@ -2,19 +2,15 @@
 /**
  * @version: $Id$
  * @package: SobiPro Library
-
  * @author
  * Name: Sigrid Suski & Radek Suski, Sigsiu.NET GmbH
  * Email: sobi[at]sigsiu.net
  * Url: http://www.Sigsiu.NET
-
  * @copyright Copyright (C) 2006 - 2013 Sigsiu.NET GmbH (http://www.sigsiu.net). All rights reserved.
  * @license GNU/LGPL Version 3
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License version 3 as published by the Free Software Foundation, and under the additional terms according section 7 of GPL v3.
  * See http://www.gnu.org/licenses/lgpl.html and http://sobipro.sigsiu.net/licenses.
-
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-
  * $Date$
  * $Revision$
  * $Author$
@@ -44,8 +40,21 @@ class SPListingCtrl extends SPSectionCtrl
 		if ( $class ) {
 			$imp = class_implements( $class );
 			if ( is_array( $imp ) && in_array( 'SPListing', $imp ) ) {
+				/** @noinspection PhpIncludeInspection $compatibility */
 				$listing = new $class();
-				$listing->setTask( $this->_task );
+				if ( !( isset( $class::$compatibility ) ) ) {
+					define( 'SOBI_LEGACY_LISTING', true );
+					if ( strstr( $this->_task, '.' ) ) {
+						$t = explode( '.', $this->_task );
+						$listing->setTask( $t[ 0 ] );
+					}
+					else {
+						$listing->setTask( $this->_task );
+					}
+				}
+				else {
+					$listing->setTask( $this->_task );
+				}
 				return $listing->execute();
 			}
 			else {
