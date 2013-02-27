28f9504f8949bb452a9eb3c8db39803ca38a8763
Radek Suski <suski.radek@gmail.com>
2013-02-27 13:02:10 +0000

    (#) Cache not cleaned after an ACL rule has been saved    (#) Empty values for range search passed to the search method ( Issue #806 )

--- Component/branches/SobiPro-1.1/Site/opt/fields/inbox.php
+++ Component/branches/SobiPro-1.1/Site/opt/fields/inbox.php
@@ -2,19 +2,15 @@
 /**
  * @version: $Id$
  * @package: SobiPro Component for Joomla!
-
  * @author
  * Name: Sigrid Suski & Radek Suski, Sigsiu.NET GmbH
  * Email: sobi[at]sigsiu.net
  * Url: http://www.Sigsiu.NET
-
  * @copyright Copyright (C) 2006 - 2013 Sigsiu.NET GmbH (http://www.sigsiu.net). All rights reserved.
  * @license GNU/GPL Version 3
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License version 3 as published by the Free Software Foundation, and under the additional terms according section 7 of GPL v3.
  * See http://www.gnu.org/licenses/gpl.html and http://sobipro.sigsiu.net/licenses.
-
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
-
  * $Date$
  * $Revision$
  * $Author$
@@ -64,7 +60,8 @@ class SPField_Inbox extends SPFieldType implements SPFieldInterface
 			return false;
 		}
 		$class = $this->required ? $this->cssClass . ' required' : $this->cssClass;
-		$params = array( 'id' => $this->nid, /*'size' => $this->width,*/ 'class' => $class );
+		$params = array( 'id' => $this->nid, /*'size' => $this->width,*/
+			'class' => $class );
 		if ( $this->maxLength ) {
 			$params[ 'maxlength' ] = $this->maxLength;
 		}
@@ -347,7 +344,7 @@ class SPField_Inbox extends SPFieldType implements SPFieldInterface
 	  */
 	public function searchData( $request, $section )
 	{
-		if ( is_array( $request ) && ( isset( $request[ 'from' ] ) || isset( $request[ 'to' ] ) ) ) {
+		if ( is_array( $request ) && ( isset( $request[ 'from' ] ) || isset( $request[ 'to' ] ) ) && ( $request[ 'from' ] || $request[ 'to' ] ) ) {
 			return $this->searchForRange( $request, $section );
 		}
 		else {
