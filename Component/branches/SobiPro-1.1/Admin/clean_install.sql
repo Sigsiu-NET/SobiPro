f51cf0a8db4553e36f1fb3221695e352c500e159
Radek Suski <suski.radek@gmail.com>
2013-03-28 15:10:57 +0000

    ENGINE = MyISAM

--- Component/branches/SobiPro-1.1/Admin/clean_install.sql
+++ Component/branches/SobiPro-1.1/Admin/clean_install.sql
@@ -178,8 +178,9 @@ CREATE TABLE IF NOT EXISTS `#__sobipro_field_data` (
 	PRIMARY KEY (`fid`, `section`, `lang`, `sid`, `copy`),
 	KEY `enabled` (`enabled`),
 	KEY `copy` (`copy`),
-	FULLTEXT KEY `baseData` (`baseData`)
+	FULLTEXT KEY `baseData` (`baseData`),
 )
+	ENGINE = MyISAM
 	DEFAULT CHARSET = utf8;
 
 CREATE TABLE IF NOT EXISTS `#__sobipro_field_option` (
