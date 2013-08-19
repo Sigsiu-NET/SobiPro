ee8292c7f45acb50e924a65b910ff6ea55eaa93d
Radek Suski <suski.radek@gmail.com>
2013-08-19 08:36:40 +0000

    Bug #966 again

--- Component/branches/SobiPro-1.1/Site/lib/models/field.php
+++ Component/branches/SobiPro-1.1/Site/lib/models/field.php
@@ -2,21 +2,17 @@
 /**
  * @version: $Id$
  * @package: SobiPro Library
-
  * @author
  * Name: Sigrid Suski & Radek Suski, Sigsiu.NET GmbH
  * Email: sobi[at]sigsiu.net
  * Url: http://www.Sigsiu.NET
-
  * @copyright Copyright (C) 2006 - 2013 Sigsiu.NET GmbH (http://www.sigsiu.net). All rights reserved.
  * @license GNU/LGPL Version 3
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License version 3
  * as published by the Free Software Foundation, and under the additional terms according section 7 of GPL v3.
  * See http://www.gnu.org/licenses/lgpl.html and http://sobipro.sigsiu.net/licenses.
-
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-
  * $Date$
  * $Revision$
  * $Author$
@@ -577,6 +573,9 @@ class SPField extends SPObject
 
 			// if the field has own method we have to re-init
 			$this->checkMethod( 'loadData' );
+			if ( $this->_type && method_exists( $this->_type, 'loadData' ) ) {
+				$this->_type->loadData( $sid );
+			}
 			if ( $this->editLimit > 0 && is_numeric( $this->_fData->editLimit ) ) {
 				$this->editLimit = $this->_fData->editLimit;
 			}
@@ -601,6 +600,9 @@ class SPField extends SPObject
 			}
 			else {
 				$this->checkMethod( 'loadData' );
+				if ( $this->_type && method_exists( $this->_type, 'loadData' ) ) {
+					$this->_type->loadData( $sid );
+				}
 			}
 		}
 		if ( !( $this->isFree ) && SPRequest::task() == 'entry.edit' ) {
