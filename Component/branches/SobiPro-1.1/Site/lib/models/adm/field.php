e776ad4d062a763b343ebe767fc6590eb0f80428
Radek Suski <Radek Suski@42bd815b-6740-4516-9028-ea9f7a097aa0>
2013-11-29 08:23:08 +0000

    (#) Preventing invalid category id in category field when set to fixed choice (Issue #1075)

--- Component/branches/SobiPro-1.1/Site/lib/models/adm/field.php
+++ Component/branches/SobiPro-1.1/Site/lib/models/adm/field.php
@@ -2,19 +2,15 @@
 /**
  * @version: $Id$
  * @package: SobiPro Library
-
  * @author
  * Name: Sigrid Suski & Radek Suski, Sigsiu.NET GmbH
  * Email: sobi[at]sigsiu.net
  * Url: http://www.Sigsiu.NET
-
  * @copyright Copyright (C) 2006 - 2013 Sigsiu.NET GmbH (http://www.sigsiu.net). All rights reserved.
  * @license GNU/LGPL Version 3
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License version 3 as published by the Free Software Foundation, and under the additional terms according section 7 of GPL v3.
  * See http://www.gnu.org/licenses/lgpl.html and http://sobipro.sigsiu.net/licenses.
-
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-
  * $Date$
  * $Revision$
  * $Author$
@@ -76,7 +72,7 @@ final class SPAdmField extends SPField
 			$base[ 'inSearch' ] = ( int )$attr[ 'inSearch' ];
 		if ( isset( $attr[ 'editLimit' ] ) )
 			$base[ 'editLimit' ] = ( int )$attr[ 'editLimit' ];
-		$base[ 'editLimit' ] = $base[ 'editLimit' ] > 0 ? $base[ 'editLimit' ] : -1;
+		$base[ 'editLimit' ] = isset( $base[ 'editLimit' ] ) && $base[ 'editLimit' ] > 0 ? $base[ 'editLimit' ] : -1;
 		if ( isset( $attr[ 'isFree' ] ) )
 			$base[ 'isFree' ] = ( int )$attr[ 'isFree' ];
 		if ( isset( $attr[ 'withLabel' ] ) )
--- Component/branches/SobiPro-1.1/Site/opt/fields/adm/category.php
+++ Component/branches/SobiPro-1.1/Site/opt/fields/adm/category.php
@@ -2,19 +2,15 @@
 /**
  * @version: $Id$
  * @package: SobiPro Component for Joomla!
-
  * @author
  * Name: Sigrid Suski & Radek Suski, Sigsiu.NET GmbH
  * Email: sobi[at]sigsiu.net
  * Url: http://www.Sigsiu.NET
-
  * @copyright Copyright (C) 2006 - 2013 Sigsiu.NET GmbH (http://www.sigsiu.net). All rights reserved.
  * @license GNU/GPL Version 3
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License version 3 as published by the Free Software Foundation, and under the additional terms according section 7 of GPL v3.
  * See http://www.gnu.org/licenses/gpl.html and http://sobipro.sigsiu.net/licenses.
-
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
-
  * $Date$
  * $Revision$
  * $Author$
@@ -35,4 +31,33 @@ class SPField_CategoryAdm extends SPField_Category
 	 * @var string
 	 */
 	public $cssClass = "inputbox";
+
+	public function save( &$attr )
+	{
+		if ( $attr[ 'method' ] == 'fixed' ) {
+			if ( !( $attr[ 'fixedCid' ] ) ) {
+				throw new SPException( SPLang::e( 'FIELD_FIXED_CID_MISSING' ) );
+			}
+			else {
+				$cids = explode( ',', $attr[ 'fixedCid' ] );
+				if ( count( $cids ) ) {
+					foreach ( $cids as $cid ) {
+						$catId = (int)$cid;
+						if ( !( $catId ) ) {
+							throw new SPException( SPLang::e( 'FIELD_FIXED_CID_INVALID', $cid ) );
+						}
+						else {
+							$parents = SPFactory::config()->getParentPath( $catId );
+							if ( !( isset( $parents[ 0 ] ) ) || $parents[ 0 ] != Sobi::Section() ) {
+								throw new SPException( SPLang::e( 'FIELD_FIXED_CID_INVALID_SECTION', $catId ) );
+							}
+						}
+					}
+				}
+				else {
+					throw new SPException( SPLang::e( 'FIELD_FIXED_CID_MISSING' ) );
+				}
+			}
+		}
+	}
 }
