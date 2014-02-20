77724efda304ac0ea903e490b0d98caa1504d98d
Sigrid Suski <SigsiuTrinity@users.noreply.github.com>
2014-02-20 16:00:03 +0000

    uncompressed js files separated

---
+++ Component/branches/SobiPro-1.1/uncompressed/js/adm/front.js
@@ -0,0 +1,58 @@
+/**
+ * @version: $Id$
+ * @package: SobiPro Library
+
+ * @author
+ * Name: Sigrid Suski & Radek Suski, Sigsiu.NET GmbH
+ * Email: sobi[at]sigsiu.net
+ * Url: http://www.Sigsiu.NET
+
+ * @copyright Copyright (C) 2006 - 2013 Sigsiu.NET GmbH (http://www.sigsiu.net). All rights reserved.
+ * @license GNU/LGPL Version 3
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License version 3 as published by the Free Software Foundation, and under the additional terms according section 7 of GPL v3.
+ * See http://www.gnu.org/licenses/lgpl.html and http://sobipro.sigsiu.net/licenses.
+
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+ * $Date$
+ * $Revision$
+ * $Author$
+ * $HeadURL$
+ */
+
+var SPUpdSemaphor = 0;
+SobiPro.jQuery( document ).ready( function ()
+{
+	SobiPro.jQuery( '#SobiProUpdates' ).on( 'shown', function ()
+	{
+		if ( SPUpdSemaphor == 0 ) {
+			SPUpdSemaphor = 1;
+			SobiPro.jQuery( '#SPVerUpd' ).html( SobiPro.Txt( 'CHECKING_FOR_UPDATES' ) + '&nbsp;<img src="../media/sobipro/adm/progress.gif"/>' );
+			SobiPro.jQuery.ajax( {
+				url:SobiProAdmUrl.replace( '%task%', 'extensions.updates' ),
+				dataType:'json',
+				success:function ( updates )
+				{
+					"use strict";
+					if ( updates.err ) {
+						SobiPro.jQuery( '#SPVerUpd' ).html( '<span style="color:#C60000; font-weight:bold;">' + updates.err + '</span>' );
+					}
+					else {
+						var output = '';
+						for ( var x in updates ) {
+							var name = updates[ x ].name + ' (' + updates[ x ].type + ') ';
+							if ( updates[ x ].update == 'false' ) {
+								var state = '&nbsp;<span style="color:#008000;">' + updates[ x ].update_txt + '</span>';
+							}
+							else {
+								var state = '&nbsp;<span style="color:#C60000; font-weight: bold">' + updates[ x ].update_txt + '</span>';
+							}
+							output += '<div class="spUpdatesApp">' + name + '</div><div class="spUpdatesState"> ' + state + '</div>';
+						}
+						SobiPro.jQuery( '#SPVerUpd' ).html( output + '<div class="clearfix"></div>' );
+					}
+				}
+			} );
+		}
+	} )
+} );
---
+++ Component/branches/SobiPro-1.1/uncompressed/js/adm/jnmenu.js
@@ -0,0 +1,230 @@
+/**
+ * @version: $Id$
+ * @package: SobiPro Library
+
+ * @author
+ * Name: Sigrid Suski & Radek Suski, Sigsiu.NET GmbH
+ * Email: sobi[at]sigsiu.net
+ * Url: http://www.Sigsiu.NET
+
+ * @copyright Copyright (C) 2006 - 2013 Sigsiu.NET GmbH (http://www.sigsiu.net). All rights reserved.
+ * @license GNU/LGPL Version 3
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License version 3 as published by the Free Software Foundation, and under the additional terms according section 7 of GPL v3.
+ * See http://www.gnu.org/licenses/lgpl.html and http://sobipro.sigsiu.net/licenses.
+
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+ * $Date$
+ * $Revision$
+ * $Author$
+ * $HeadURL$
+ */
+
+var SobiProUrl = '';
+SobiPro.jQuery( document ).ready( function ()
+{
+	var modal = SobiPro.jQuery( '#SobiProModal' );
+	SobiPro.jQuery( '#jform_link' ).css( 'min-width', '400px' );
+	modal.find( '.ctrl-save' ).click( function ( e )
+	{
+		e.preventDefault();
+		var fields = modal.find( 'iframe' ).contents().find( ':input' );
+		var setting = {};
+		var request = [];
+		SobiPro.jQuery.each( fields, function ( i, e )
+		{
+			e = SobiPro.jQuery( e );
+			switch ( e.attr( 'name' ) ) {
+				case 'sid':
+					SobiPro.jQuery( '#selectedSid' ).val( e.val() );
+					break;
+				case 'function-name':
+					var functionName = e.val();
+					if ( functionName.indexOf( '%s' ) != -1 ) {
+						functionName = functionName.replace( '%s', SobiPro.jQuery( "#SobiSection" ).find( ':selected' ).text() );
+					}
+					SobiPro.jQuery( '#SobiProSelectedFunction' ).html( functionName );
+					break;
+				default:
+					if ( e.attr( 'name' ).indexOf( 'params' ) != -1 ) {
+						var name = e.attr( 'name' ).replace( 'params[', '' ).replace( ']', '' );
+						setting[ name ] = e.val();
+					}
+					else if ( e.attr( 'name' ).indexOf( 'request' ) != -1 ) {
+						var name = e.attr( 'name' ).replace( 'request[', '' ).replace( ']', '' );
+						request.push( { 'name': name, 'value': e.val() } );
+					}
+					break;
+			}
+		} );
+		if ( request.length ) {
+			var link = 'index.php?option=com_sobipro&';
+			SobiPro.jQuery.each( request, function ( i, e )
+			{
+				if ( e.value.length ) {
+					link = link + e.name + '=' + e.value;
+					if ( i + 1 < request.length ) {
+						link = link + '&';
+					}
+				}
+			} );
+			SobiProUrl = link;
+			SPUpdateUrl();
+		}
+		SobiPro.jQuery( '.SobiProSettings' ).val( SobiPro.jQuery.base64.encode( JSON.stringify( setting ) ) );
+	} );
+
+	modal.find( '.ctrl-clear' ).click( function ( e )
+	{
+		SobiProUrl = '';
+		SPUpdateUrl();
+		SobiPro.jQuery( '#SobiProSelectedFunction' ).html( SpStrings.buttonLabel )
+	} );
+
+	SobiPro.jQuery( '#SobiProSelector' ).bind( 'click', function ( e )
+	{
+		if ( SobiPro.jQuery( "#SobiSection" ).val() == 0 || SobiPro.jQuery( "#SobiSection" ).val() == "" ) {
+			SobiPro.Alert( 'YOU_HAVE_TO_AT_LEAST_SELECT_A_SECTION' );
+			return false;
+		}
+		modal.modal();
+		var mid = SobiPro.jQuery( '#SobiProSelector' ).data( 'mid' );
+		var section = SobiPro.jQuery( '#SobiSection' ).val();
+		modal.find( '.modal-body' )
+			.html( '<iframe src="index.php?option=com_sobipro&section=' + section + '&task=menu&tmpl=component&format=html&mid=' + mid + '" style="width: 480px; height: 400px; border: none;"> </iframe>' );
+	} );
+
+	try {
+		var JSubmit = Joomla.submitbutton;
+		Joomla.submitbutton = function ( pressbutton, type )
+		{
+			if ( pressbutton.indexOf( 'save' ).indexOf == -1 || pressbutton.indexOf( 'apply' ) == -1 || SPValidate() ) {
+				JSubmit( pressbutton, type );
+			}
+		}
+	}
+	catch ( x ) {
+		var JSubmit = submitbutton;
+		submitbutton = function ( pressbutton, type )
+		{
+			if ( pressbutton.indexOf( 'save' ).indexOf == -1 || pressbutton.indexOf( 'apply' ) == -1 || SPValidate() ) {
+				JSubmit( pressbutton, type );
+			}
+		}
+	}
+	SobiPro.jQuery( '#SobiProFunctions' ).change( function ()
+	{
+		if ( SobiPro.jQuery( this ).val() ) {
+			SobiPro.jQuery( 'body' ).find( 'form' ).submit();
+		}
+	} );
+
+	SobiPro.jQuery( '.SobiProCalendar' ).change( function ()
+		{
+			"use strict";
+			var date = [];
+			SobiPro.jQuery( '.SobiProCalendar' ).each( function ( i, e )
+			{
+				if ( SobiPro.jQuery( this ).val() ) {
+					date.push( SobiPro.jQuery( this ).val() );
+				}
+			} );
+			SobiPro.jQuery( '[name=request\\[date\\]]' ).val( date.join( '.' ) );
+		}
+	);
+
+	SobiPro.jQuery( '.SobiProEntryChooser' ).typeahead( {
+		source: function ( typeahead, query )
+		{
+			var request = {
+				'option': 'com_sobipro',
+				'task': 'entry.search',
+				'sid': SobiPro.jQuery( '#SP_section' ).val(),
+				'search': query,
+				'format': 'raw'
+			};
+			return SobiPro.jQuery.ajax( {
+				'type': 'post',
+				'url': 'index.php',
+				'data': request,
+				'dataType': 'json',
+				success: function ( response )
+				{
+					responseData = [];
+					if ( response.length ) {
+						for ( var i = 0; i < response.length; i++ ) {
+							responseData[ i ] = { 'name': response[ i ].name + ' ( ' + response[ i ].id + ' )', 'id': response[ i ].id, 'title': response[ i ].name  };
+						}
+						typeahead.process( responseData );
+					}
+				}
+			} );
+		},
+		onselect: function ( obj )
+		{
+			SobiPro.jQuery( '#SP_sid' ).val( obj.id );
+			SobiPro.jQuery( '#selectedEntryName' ).val( obj.title );
+			SobiPro.jQuery( 'input[name=function-name]' ).val( SobiPro.jQuery( '#entryName' ).html().replace( '%s', obj.title ) );
+		},
+		property: "name"
+	} );
+	try {
+		SobiPro.jQuery( SobiPro.jQuery( '#jform_type-lbl' ).siblings()[ 0 ] ).val( SpStrings.component );
+	}
+	catch ( e ) {
+	}
+	try {
+		var lType = SobiPro.jQuery( '[name*="jform[type]"]' ).parent().find( 'input[type=text]' );
+		lType.val( SpStrings.component );
+		lType.css( 'min-width', '200px' );
+		SobiPro.jQuery( '#jform_link' ).css( 'min-width', '500px' );
+	}
+	catch ( e ) {
+	}
+} );
+
+function SP_selectCat( sid )
+{
+	SobiPro.jQuery( 'input[name*=sid]' ).val( sid );
+	SobiPro.jQuery( '#sobiCats_CatUrl' + sid ).focus();
+	SobiPro.jQuery.ajax( {
+		'url': 'index.php',
+		'data': { 'task': 'category.parents', 'out': 'json', 'option': 'com_sobipro', 'format': 'raw', 'tmpl': 'component', 'sid': sid },
+		'type': 'post',
+		'dataType': 'json'
+	} ).done(
+		function ( response )
+		{
+			var catName = '';
+			SobiPro.jQuery.each( response.categories, function ( i, cat )
+			{
+				catName = cat.name;
+			} );
+			SobiPro.jQuery( 'input[name=function-name]' ).val( SobiPro.jQuery( '#categoryName' ).html().replace( '%s', catName ) );
+		}
+	);
+}
+
+function SPUpdateUrl()
+{
+	if ( !( SobiPro.jQuery( '#selectedSid' ).val() ) ) {
+		SobiPro.jQuery( '#selectedSid' ).val( SobiPro.jQuery( '#SobiSection' ).val() );
+	}
+	if ( SobiProUrl == '' ) {
+		SobiProUrl = 'index.php?option=com_sobipro&sid=' + SobiPro.jQuery( '#selectedSid' ).val();
+		SobiPro.jQuery( '.SobiProSettings' ).val( '' );
+	}
+	SobiPro.jQuery( '#jform_link' ).val( SobiProUrl );
+}
+
+function SPValidate()
+{
+	SPUpdateUrl();
+	if ( SobiPro.jQuery( "#selectedSid" ).val() == 0 || SobiPro.jQuery( "#selectedSid" ).val() == "" ) {
+		SobiPro.Alert( 'YOU_HAVE_TO_AT_LEAST_SELECT_A_SECTION' );
+		return false;
+	}
+	else {
+		return true;
+	}
+}
