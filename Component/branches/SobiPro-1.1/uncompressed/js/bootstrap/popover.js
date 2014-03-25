56ba0c40eb6993b84675cd4d85212e35a904bbc8
Radek Suski <Radek Suski@42bd815b-6740-4516-9028-ea9f7a097aa0>
2014-03-25 11:14:59 +0000

    (+) Possibility to delete a date form datetimepicker (Issue #1208)

--- Component/branches/SobiPro-1.1/uncompressed/js/bootstrap/popover.js
+++ Component/branches/SobiPro-1.1/uncompressed/js/bootstrap/popover.js
@@ -26,72 +26,70 @@
     /* POPOVER PUBLIC CLASS DEFINITION
      * =============================== */
 
-    var Popover = function (element, options) {
-        this.init('popover', element, options)
-    }
+    var Popover = function ( element, options )
+    {
+	    this.init( 'popover', element, options )
+    };
 
 
     /* NOTE: POPOVER EXTENDS BOOTSTRAP-TOOLTIP.js
      ========================================== */
 
-    Popover.prototype = $.extend({}, $.fn.tooltip.Constructor.prototype, {
+    Popover.prototype = $.extend( {}, $.fn.tooltip.Constructor.prototype, {
 
-        constructor: Popover
+	    constructor: Popover, setContent: function ()
+	    {
+		    var $tip = this.tip()
+			    , title = this.getTitle()
+			    , content = this.getContent();
 
-        , setContent: function () {
-            var $tip = this.tip()
-                , title = this.getTitle()
-                , content = this.getContent()
+		    $tip.find( '.popover-title' )[this.options.html ? 'html' : 'text']( title );
+		    $tip.find( '.popover-content > *' )[this.options.html ? 'html' : 'text']( content );
 
-            $tip.find('.popover-title')[this.options.html ? 'html' : 'text'](title)
-            $tip.find('.popover-content > *')[this.options.html ? 'html' : 'text'](content)
+		    $tip.removeClass( 'fade top bottom left right in' )
+	    }, hasContent: function ()
+	    {
+		    return this.getTitle() || this.getContent()
+	    }, getContent: function ()
+	    {
+		    var content
+			    , $e = this.$element
+			    , o = this.options;
 
-            $tip.removeClass('fade top bottom left right in')
-        }
+		    content = $e.attr( 'data-content' )
+			    || (typeof o.content == 'function' ? o.content.call( $e[0] ) : o.content);
 
-        , hasContent: function () {
-            return this.getTitle() || this.getContent()
-        }
+		    return content
+	    }, tip: function ()
+	    {
+		    if ( !this.$tip ) {
+			    this.$tip = $( this.options.template )
+		    }
+		    return this.$tip
+	    }, destroy: function ()
+	    {
+		    this.hide().$element.off( '.' + this.type ).removeData( this.type )
+	    }
 
-        , getContent: function () {
-            var content
-                , $e = this.$element
-                , o = this.options
-
-            content = $e.attr('data-content')
-                || (typeof o.content == 'function' ? o.content.call($e[0]) :  o.content)
-
-            return content
-        }
-
-        , tip: function () {
-            if (!this.$tip) {
-                this.$tip = $(this.options.template)
-            }
-            return this.$tip
-        }
-
-        , destroy: function () {
-            this.hide().$element.off('.' + this.type).removeData(this.type)
-        }
-
-    })
+    } );
 
 
     /* POPOVER PLUGIN DEFINITION
      * ======================= */
 
-    $.fn.popover = function (option) {
-        return this.each(function () {
-            var $this = $(this)
-                , data = $this.data('popover')
-                , options = typeof option == 'object' && option
-            if (!data) $this.data('popover', (data = new Popover(this, options)))
-            if (typeof option == 'string') data[option]()
-        })
-    }
+    $.fn.popover = function ( option )
+    {
+	    return this.each( function ()
+	    {
+		    var $this = $( this )
+			    , data = $this.data( 'popover' )
+			    , options = typeof option == 'object' && option;
+		    if ( !data ) $this.data( 'popover', (data = new Popover( this, options )) );
+		    if ( typeof option == 'string' ) data[option]()
+	    } )
+    };
 
-    $.fn.popover.Constructor = Popover
+    $.fn.popover.Constructor = Popover;
 
     $.fn.popover.defaults = $.extend({} , $.fn.tooltip.defaults, {
         placement: 'right'
--- Component/branches/SobiPro-1.1/uncompressed/js/bootstrap/tooltips.js
+++ Component/branches/SobiPro-1.1/uncompressed/js/bootstrap/tooltips.js
@@ -27,240 +27,229 @@
  /* TOOLTIP PUBLIC CLASS DEFINITION
   * =============================== */
 
-  var Tooltip = function (element, options) {
-    this.init('tooltip', element, options)
-  }
+  var Tooltip = function ( element, options )
+  {
+	  this.init( 'tooltip', element, options )
+  };
 
   Tooltip.prototype = {
 
-    constructor: Tooltip
+	  constructor: Tooltip, init: function ( type, element, options )
+	  {
+		  var eventIn
+			  , eventOut;
 
-  , init: function (type, element, options) {
-      var eventIn
-        , eventOut
+		  this.type = type;
+		  this.$element = $( element );
+		  this.options = this.getOptions( options );
+		  this.enabled = true;
 
-      this.type = type
-      this.$element = $(element)
-      this.options = this.getOptions(options)
-      this.enabled = true
+		  if ( this.options.trigger == 'click' ) {
+			  this.$element.on( 'click.' + this.type, this.options.selector, $.proxy( this.toggle, this ) )
+		  }
+		  else if ( this.options.trigger != 'manual' ) {
+			  eventIn = this.options.trigger == 'hover' ? 'mouseenter' : 'focus';
+			  eventOut = this.options.trigger == 'hover' ? 'mouseleave' : 'blur';
+			  this.$element.on( eventIn + '.' + this.type, this.options.selector, $.proxy( this.enter, this ) );
+			  this.$element.on( eventOut + '.' + this.type, this.options.selector, $.proxy( this.leave, this ) )
+		  }
 
-      if (this.options.trigger == 'click') {
-        this.$element.on('click.' + this.type, this.options.selector, $.proxy(this.toggle, this))
-      } else if (this.options.trigger != 'manual') {
-        eventIn = this.options.trigger == 'hover' ? 'mouseenter' : 'focus'
-        eventOut = this.options.trigger == 'hover' ? 'mouseleave' : 'blur'
-        this.$element.on(eventIn + '.' + this.type, this.options.selector, $.proxy(this.enter, this))
-        this.$element.on(eventOut + '.' + this.type, this.options.selector, $.proxy(this.leave, this))
-      }
+		  this.options.selector ?
+			  (this._options = $.extend( {}, this.options, { trigger: 'manual', selector: '' } )) :
+			  this.fixTitle()
+	  }, getOptions: function ( options )
+	  {
+		  options = $.extend( {}, $.fn[this.type].defaults, options, this.$element.data() );
 
-      this.options.selector ?
-        (this._options = $.extend({}, this.options, { trigger: 'manual', selector: '' })) :
-        this.fixTitle()
-    }
+		  if ( options.delay && typeof options.delay == 'number' ) {
+			  options.delay = {
+				  show: options.delay, hide: options.delay
+			  }
+		  }
 
-  , getOptions: function (options) {
-      options = $.extend({}, $.fn[this.type].defaults, options, this.$element.data())
+		  return options
+	  }, enter: function ( e )
+	  {
+		  var self = $( e.currentTarget )[this.type]( this._options ).data( this.type );
 
-      if (options.delay && typeof options.delay == 'number') {
-        options.delay = {
-          show: options.delay
-        , hide: options.delay
-        }
-      }
+		  if ( !self.options.delay || !self.options.delay.show ) return self.show();
 
-      return options
-    }
+		  clearTimeout( this.timeout );
+		  self.hoverState = 'in';
+		  this.timeout = setTimeout( function ()
+		  {
+			  if ( self.hoverState == 'in' ) self.show()
+		  }, self.options.delay.show )
+	  }, leave: function ( e )
+	  {
+		  var self = $( e.currentTarget )[this.type]( this._options ).data( this.type );
 
-  , enter: function (e) {
-      var self = $(e.currentTarget)[this.type](this._options).data(this.type)
+		  if ( this.timeout ) clearTimeout( this.timeout );
+		  if ( !self.options.delay || !self.options.delay.hide ) return self.hide();
 
-      if (!self.options.delay || !self.options.delay.show) return self.show()
+		  self.hoverState = 'out';
+		  this.timeout = setTimeout( function ()
+		  {
+			  if ( self.hoverState == 'out' ) self.hide()
+		  }, self.options.delay.hide )
+	  }, show: function ()
+	  {
+		  var $tip
+			  , inside
+			  , pos
+			  , actualWidth
+			  , actualHeight
+			  , placement
+			  , tp;
 
-      clearTimeout(this.timeout)
-      self.hoverState = 'in'
-      this.timeout = setTimeout(function() {
-        if (self.hoverState == 'in') self.show()
-      }, self.options.delay.show)
-    }
+		  if ( this.hasContent() && this.enabled ) {
+			  $tip = this.tip();
+			  this.setContent();
 
-  , leave: function (e) {
-      var self = $(e.currentTarget)[this.type](this._options).data(this.type)
+			  if ( this.options.animation ) {
+				  $tip.addClass( 'fade' )
+			  }
 
-      if (this.timeout) clearTimeout(this.timeout)
-      if (!self.options.delay || !self.options.delay.hide) return self.hide()
+			  placement = typeof this.options.placement == 'function' ?
+				  this.options.placement.call( this, $tip[0], this.$element[0] ) :
+				  this.options.placement;
 
-      self.hoverState = 'out'
-      this.timeout = setTimeout(function() {
-        if (self.hoverState == 'out') self.hide()
-      }, self.options.delay.hide)
-    }
+			  inside = /in/.test( placement );
 
-  , show: function () {
-      var $tip
-        , inside
-        , pos
-        , actualWidth
-        , actualHeight
-        , placement
-        , tp
+			  $tip
+				  .detach()
+				  .css( { top: 0, left: 0, display: 'block' } )
+				  .insertAfter( this.$element );
 
-      if (this.hasContent() && this.enabled) {
-        $tip = this.tip()
-        this.setContent()
+			  pos = this.getPosition( inside );
 
-        if (this.options.animation) {
-          $tip.addClass('fade')
-        }
+			  actualWidth = $tip[0].offsetWidth;
+			  actualHeight = $tip[0].offsetHeight;
 
-        placement = typeof this.options.placement == 'function' ?
-          this.options.placement.call(this, $tip[0], this.$element[0]) :
-          this.options.placement
+			  switch ( inside ? placement.split( ' ' )[1] : placement ) {
+				  case 'bottom':
+					  tp = {top: pos.top + pos.height, left: pos.left + pos.width / 2 - actualWidth / 2};
+					  break;
+				  case 'top':
+					  tp = {top: pos.top - actualHeight, left: pos.left + pos.width / 2 - actualWidth / 2};
+					  break;
+				  case 'left':
+					  tp = {top: pos.top + pos.height / 2 - actualHeight / 2, left: pos.left - actualWidth};
+					  break;
+				  case 'right':
+					  tp = {top: pos.top + pos.height / 2 - actualHeight / 2, left: pos.left + pos.width};
+					  break
+			  }
 
-        inside = /in/.test(placement)
+			  $tip
+				  .offset( tp )
+				  .addClass( placement )
+				  .addClass( 'in' )
+		  }
+	  }, setContent: function ()
+	  {
+		  var $tip = this.tip()
+			  , title = this.getTitle();
 
-        $tip
-          .detach()
-          .css({ top: 0, left: 0, display: 'block' })
-          .insertAfter(this.$element)
+		  $tip.find( '.tooltip-inner' )[this.options.html ? 'html' : 'text']( title );
+		  $tip.removeClass( 'fade in top bottom left right' )
+	  }, hide: function ()
+	  {
+		  var that = this
+			  , $tip = this.tip();
 
-        pos = this.getPosition(inside)
+		  $tip.removeClass( 'in' );
 
-        actualWidth = $tip[0].offsetWidth
-        actualHeight = $tip[0].offsetHeight
+		  function removeWithAnimation()
+		  {
+			  var timeout = setTimeout( function ()
+			  {
+				  $tip.off( $.support.transition.end ).detach()
+			  }, 500 );
 
-        switch (inside ? placement.split(' ')[1] : placement) {
-          case 'bottom':
-            tp = {top: pos.top + pos.height, left: pos.left + pos.width / 2 - actualWidth / 2}
-            break
-          case 'top':
-            tp = {top: pos.top - actualHeight, left: pos.left + pos.width / 2 - actualWidth / 2}
-            break
-          case 'left':
-            tp = {top: pos.top + pos.height / 2 - actualHeight / 2, left: pos.left - actualWidth}
-            break
-          case 'right':
-            tp = {top: pos.top + pos.height / 2 - actualHeight / 2, left: pos.left + pos.width}
-            break
-        }
+			  $tip.one( $.support.transition.end, function ()
+			  {
+				  clearTimeout( timeout );
+				  $tip.detach()
+			  } )
+		  }
 
-        $tip
-          .offset(tp)
-          .addClass(placement)
-          .addClass('in')
-      }
-    }
+		  $.support.transition && this.$tip.hasClass( 'fade' ) ?
+			  removeWithAnimation() :
+			  $tip.detach();
 
-  , setContent: function () {
-      var $tip = this.tip()
-        , title = this.getTitle()
+		  return this
+	  }, fixTitle: function ()
+	  {
+		  var $e = this.$element;
+		  if ( $e.attr( 'title' ) || typeof($e.attr( 'data-original-title' )) != 'string' ) {
+			  $e.attr( 'data-original-title', $e.attr( 'title' ) || '' ).removeAttr( 'title' )
+		  }
+	  }, hasContent: function ()
+	  {
+		  return this.getTitle()
+	  }, getPosition: function ( inside )
+	  {
+		  return $.extend( {}, (inside ? {top: 0, left: 0} : this.$element.offset()), {
+			  width: this.$element[0].offsetWidth, height: this.$element[0].offsetHeight
+		  } )
+	  }, getTitle: function ()
+	  {
+		  var title
+			  , $e = this.$element
+			  , o = this.options;
 
-      $tip.find('.tooltip-inner')[this.options.html ? 'html' : 'text'](title)
-      $tip.removeClass('fade in top bottom left right')
-    }
+		  title = $e.attr( 'data-original-title' )
+			  || (typeof o.title == 'function' ? o.title.call( $e[0] ) : o.title);
 
-  , hide: function () {
-      var that = this
-        , $tip = this.tip()
+		  return title
+	  }, tip: function ()
+	  {
+		  return this.$tip = this.$tip || $( this.options.template )
+	  }, validate: function ()
+	  {
+		  if ( !this.$element[0].parentNode ) {
+			  this.hide();
+			  this.$element = null;
+			  this.options = null
+		  }
+	  }, enable: function ()
+	  {
+		  this.enabled = true
+	  }, disable: function ()
+	  {
+		  this.enabled = false
+	  }, toggleEnabled: function ()
+	  {
+		  this.enabled = !this.enabled
+	  }, toggle: function ( e )
+	  {
+		  var self = $( e.currentTarget )[this.type]( this._options ).data( this.type );
+		  self[self.tip().hasClass( 'in' ) ? 'hide' : 'show']()
+	  }, destroy: function ()
+	  {
+		  this.hide().$element.off( '.' + this.type ).removeData( this.type )
+	  }
 
-      $tip.removeClass('in')
-
-      function removeWithAnimation() {
-        var timeout = setTimeout(function () {
-          $tip.off($.support.transition.end).detach()
-        }, 500)
-
-        $tip.one($.support.transition.end, function () {
-          clearTimeout(timeout)
-          $tip.detach()
-        })
-      }
-
-      $.support.transition && this.$tip.hasClass('fade') ?
-        removeWithAnimation() :
-        $tip.detach()
-
-      return this
-    }
-
-  , fixTitle: function () {
-      var $e = this.$element
-      if ($e.attr('title') || typeof($e.attr('data-original-title')) != 'string') {
-        $e.attr('data-original-title', $e.attr('title') || '').removeAttr('title')
-      }
-    }
-
-  , hasContent: function () {
-      return this.getTitle()
-    }
-
-  , getPosition: function (inside) {
-      return $.extend({}, (inside ? {top: 0, left: 0} : this.$element.offset()), {
-        width: this.$element[0].offsetWidth
-      , height: this.$element[0].offsetHeight
-      })
-    }
-
-  , getTitle: function () {
-      var title
-        , $e = this.$element
-        , o = this.options
-
-      title = $e.attr('data-original-title')
-        || (typeof o.title == 'function' ? o.title.call($e[0]) :  o.title)
-
-      return title
-    }
-
-  , tip: function () {
-      return this.$tip = this.$tip || $(this.options.template)
-    }
-
-  , validate: function () {
-      if (!this.$element[0].parentNode) {
-        this.hide()
-        this.$element = null
-        this.options = null
-      }
-    }
-
-  , enable: function () {
-      this.enabled = true
-    }
-
-  , disable: function () {
-      this.enabled = false
-    }
-
-  , toggleEnabled: function () {
-      this.enabled = !this.enabled
-    }
-
-  , toggle: function (e) {
-      var self = $(e.currentTarget)[this.type](this._options).data(this.type)
-      self[self.tip().hasClass('in') ? 'hide' : 'show']()
-    }
-
-  , destroy: function () {
-      this.hide().$element.off('.' + this.type).removeData(this.type)
-    }
-
-  }
+  };
 
 
  /* TOOLTIP PLUGIN DEFINITION
   * ========================= */
 
-  $.fn.tooltip = function ( option ) {
-    return this.each(function () {
-      var $this = $(this)
-        , data = $this.data('tooltip')
-        , options = typeof option == 'object' && option
-      if (!data) $this.data('tooltip', (data = new Tooltip(this, options)))
-      if (typeof option == 'string') data[option]()
-    })
-  }
+  $.fn.tooltip = function ( option )
+  {
+	  return this.each( function ()
+	  {
+		  var $this = $( this )
+			  , data = $this.data( 'tooltip' )
+			  , options = typeof option == 'object' && option;
+		  if ( !data ) $this.data( 'tooltip', (data = new Tooltip( this, options )) );
+		  if ( typeof option == 'string' ) data[option]()
+	  } )
+  };
 
-  $.fn.tooltip.Constructor = Tooltip
+  $.fn.tooltip.Constructor = Tooltip;
 
   $.fn.tooltip.defaults = {
     animation: true
