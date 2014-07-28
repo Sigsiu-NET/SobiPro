2d04c7a4f9762e61ee01f66e1aa423b0596dba19
Sigrid Suski <Sigrid Suski@42bd815b-6740-4516-9028-ea9f7a097aa0>
2014-07-28 17:37:42 +0000

    1.1.10

--- Component/branches/SobiPro-1.1/Site/lib/ctrl/adm/config.php
+++ Component/branches/SobiPro-1.1/Site/lib/ctrl/adm/config.php
@@ -6,11 +6,13 @@
  * Name: Sigrid Suski & Radek Suski, Sigsiu.NET GmbH
  * Email: sobi[at]sigsiu.net
  * Url: http://www.Sigsiu.NET
- * @copyright Copyright (C) 2006 - 2013 Sigsiu.NET GmbH (http://www.sigsiu.net). All rights reserved.
+ * @copyright Copyright (C) 2006 - 2014 Sigsiu.NET GmbH (http://www.sigsiu.net). All rights reserved.
  * @license GNU/LGPL Version 3
- * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License version 3 as published by the Free Software Foundation, and under the additional terms according section 7 of GPL v3.
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License version 3
+ * as published by the Free Software Foundation, and under the additional terms according section 7 of GPL v3.
  * See http://www.gnu.org/licenses/lgpl.html and http://sobipro.sigsiu.net/licenses.
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
  * $Date$
  * $Revision$
  * $Author$
@@ -230,14 +232,19 @@ class SPConfigAdmCtrl extends SPController
 	{
 		$target = SPRequest::cmd( 'target' );
 		$order = Sobi::GetUserState( $target . '.order', null );
+		$saved = false;
 		if ( strlen( $order ) ) {
 			SPFactory::config()->saveCfg( 'admin.' . $target . '-order', $order );
+            $saved = true;
 		}
 		$limit = Sobi::GetUserState( $target . '.limit', 10 );
 		if ( $limit ) {
 			SPFactory::config()->saveCfg( 'admin.' . $target . '-limit', $limit );
+            $saved = true;
 		}
-		$this->response( Sobi::Back(), Sobi::Txt( 'MSG_DEFAULT_ORDERING_SAVED' ), false );
+        if ($saved) {
+            $this->response( Sobi::Back(), Sobi::Txt( 'MSG_DEFAULT_ORDERING_SAVED' ), false );
+        }
 	}
 
 	/**
