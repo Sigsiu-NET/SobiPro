668df62cedb36cc0bc92a7c071740f8371ab6e1b
TrinitySigsiu <sigrid.suski@googlemail.com>
2015-03-20 14:26:53 +0100

    year corrected to 2015 in SobiPro comment output

--- Site/lib/views/view.php
+++ Site/lib/views/view.php
@@ -340,7 +340,7 @@ abstract class SPFrontView extends SPObject implements SPView
 				$time = microtime( true ) - $time;
 				SPConfig::debOut( "Memory: {$mem}<br/>Time: {$time}<br/> Queries: {$queries}" );
 			}
-			echo "\n<!-- Start of SobiPro component-->\n<div id=\"SobiPro\" class=\"SobiPro\">\n{$out}\n</div>\n<!-- End of SobiPro component Copyright (C) 2011-2014 Sigsiu.NET GmbH -->\n";
+			echo "\n<!-- Start of SobiPro component-->\n<div id=\"SobiPro\" class=\"SobiPro\">\n{$out}\n</div>\n<!-- End of SobiPro component Copyright (C) 2011-2015 Sigsiu.NET GmbH -->\n";
 		}
 		else {
 			$this->customOutput( $out );
