71dd7c7a307c97b15a0af39ebb3feeb932c5264f
Radek Suski <suski.radek@gmail.com>
2015-07-30 16:04:44 +0200

    (#) Wrong error code if page has not been found (Issue #1440)

--- Site/lib/ctrl/sobipro.php
+++ Site/lib/ctrl/sobipro.php
@@ -155,11 +155,11 @@ final class SobiProCtrl
 			try {
 				$this->route();
 			} catch ( SPException $x ) {
-				if ( defined( 'SOBI_TESTS' ) ) {
+				if ( SOBI_TESTS ) {
 					Sobi::Error( 'CoreCtrl', SPLang::e( 'Cannot route: %s.', $x->getMessage() ), SPC::ERROR, 500, __LINE__, __FILE__ );
 				}
 				else {
-					SPFactory::mainframe()->setRedirect( Sobi::Reg( 'live_site' ), SPLang::e( 'PAGE_NOT_FOUND' ), SPC::ERROR_MSG, true );
+					Sobi::Error( 'CoreCtrl', SPLang::e( 'PAGE_NOT_FOUND' ), SPC::ERROR, 404, __LINE__, __FILE__ );
 				}
 			}
 		}
