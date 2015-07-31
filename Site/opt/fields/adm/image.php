4dc266a25453da58dd6310674f54ed7a10ca665f
Radek Suski <suski.radek@gmail.com>
2015-07-31 16:14:07 +0200

    (#) Checking for width and height size in image field when crop or resize is enabled (Issue #1445)

--- Site/opt/fields/adm/image.php
+++ Site/opt/fields/adm/image.php
@@ -2,19 +2,15 @@
 /**
  * @version: $Id$
  * @package: SobiPro Component for Joomla!
-
  * @author
  * Name: Sigrid Suski & Radek Suski, Sigsiu.NET GmbH
  * Email: sobi[at]sigsiu.net
  * Url: http://www.Sigsiu.NET
-
  * @copyright Copyright (C) 2006 - 2015 Sigsiu.NET GmbH (http://www.sigsiu.net). All rights reserved.
  * @license GNU/GPL Version 3
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License version 3 as published by the Free Software Foundation, and under the additional terms according section 7 of GPL v3.
  * See http://www.gnu.org/licenses/gpl.html and http://sobipro.sigsiu.net/licenses.
-
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
-
  * $Date$
  * $Revision$
  * $Author$
@@ -29,5 +25,12 @@ SPLoader::loadClass( 'opt.fields.image' );
  * @version 1.0
  * @created 28-Nov-2009 20:06:47
  */
-class SPField_ImageAdm extends SPField_Image {
+class SPField_ImageAdm extends SPField_Image
+{
+	public function save( &$attr )
+	{
+		if ( ( $attr[ 'resize' ] || $attr[ 'crop' ] ) && !( $attr[ 'resizeWidth' ] && $attr[ 'resizeHeight' ] ) ) {
+			throw new SPException( SPLang::e( 'IMG_FIELD_RESIZE_NO_SIZE' ) );
+		}
+	}
 }
