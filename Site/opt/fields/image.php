4d634b83281c0d196512b6114d71e4c2f3b938cd
Radek Suski <suski.radek@gmail.com>
2015-10-09 14:55:48 +0200

    Issue #1494 - was failing when no orginal image was stored

--- Site/opt/fields/image.php
+++ Site/opt/fields/image.php
@@ -1,18 +1,15 @@
 <?php
 /**
  * @package: SobiPro Component for Joomla!
-
  * @author
  * Name: Sigrid Suski & Radek Suski, Sigsiu.NET GmbH
  * Email: sobi[at]sigsiu.net
  * Url: http://www.Sigsiu.NET
-
  * @copyright Copyright (C) 2006 - 2015 Sigsiu.NET GmbH (https://www.sigsiu.net). All rights reserved.
  * @license GNU/GPL Version 3
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License version 3
  * as published by the Free Software Foundation, and under the additional terms according section 7 of GPL v3.
  * See http://www.gnu.org/licenses/gpl.html and https://www.sigsiu.net/licenses.
-
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
  */
@@ -185,7 +182,7 @@ class SPField_Image extends SPField_Inbox implements SPFieldInterface
 		}
 		if ( $this->crop ) {
 			$modalclass = 'modal hide';
-			if (Sobi::Cfg( 'template.bootstrap3-styles' ) && !defined( 'SOBIPRO_ADM' )) {
+			if ( Sobi::Cfg( 'template.bootstrap3-styles' ) && !defined( 'SOBIPRO_ADM' ) ) {
 				$modalclass = 'modal fade';
 			}
 
@@ -377,7 +374,7 @@ class SPField_Image extends SPField_Inbox implements SPFieldInterface
 			$this->loadData( $orgSid );
 			$files = $this->getExistingFiles();
 			$cloneFiles = array();
-			if ( isset( $files[ 'original' ] ) && file_exists( SOBI_ROOT . '/' . $files[ 'original' ] ) ) {
+			if ( isset( $files[ 'image' ] ) && file_exists( SOBI_ROOT . '/' . $files[ 'image' ] ) ) {
 				return $this->cloneFiles( $entry, $request, $files, $cloneFiles );
 			}
 		}
@@ -971,10 +968,12 @@ class SPField_Image extends SPField_Inbox implements SPFieldInterface
 	 */
 	protected function cloneFiles( &$entry, $request, $files, $cloneFiles )
 	{
-		$orgName = basename( $files[ 'original' ] );
+		$orgName = basename( isset( $files[ 'original' ] ) ? $files[ 'original' ] : $files[ 'image' ] );
 		$sPath = $this->parseName( $entry, $orgName, $this->savePath );
-		$cloneFiles[ 'original' ] = $sPath . $this->parseName( $entry, $orgName, '{orgname}', true );
-		SPFs::copy( SOBI_ROOT . '/' . $files[ 'original' ], SOBI_ROOT . '/' . $cloneFiles[ 'original' ] );
+		if ( isset( $files[ 'original' ] ) ) {
+			$cloneFiles[ 'original' ] = $sPath . $this->parseName( $entry, $orgName, '{orgname}', true );
+			SPFs::copy( SOBI_ROOT . '/' . $files[ 'original' ], SOBI_ROOT . '/' . $cloneFiles[ 'original' ] );
+		}
 
 		$cloneFiles[ 'image' ] = $sPath . $this->parseName( $entry, $orgName, $this->imageName, true );
 		SPFs::copy( SOBI_ROOT . '/' . $files[ 'image' ], SOBI_ROOT . '/' . $cloneFiles[ 'image' ] );
