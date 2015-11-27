5f1e6b30394a8edac1b6fd4228d7a5da529fe951
Radek Suski <suski.radek@gmail.com>
2015-11-27 16:26:46 +0100

    Info field. Duplication

--- Site/opt/fields/adm/info.php
+++ Site/opt/fields/adm/info.php
@@ -1,22 +1,20 @@
 <?php
 /**
  * @package: SobiPro Component for Joomla!
-
  * @author
  * Name: Sigrid Suski & Radek Suski, Sigsiu.NET GmbH
  * Email: sobi[at]sigsiu.net
  * Url: https://www.Sigsiu.NET
-
  * @copyright Copyright (C) 2006 - 2015 Sigsiu.NET GmbH (https://www.sigsiu.net). All rights reserved.
  * @license GNU/GPL Version 3
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License version 3 as published by the Free Software Foundation, and under the additional terms according section 7 of GPL v3.
  * See http://www.gnu.org/licenses/gpl.html and httsp://www.sigsiu.net/licenses.
-
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
  */
 
 defined( 'SOBIPRO' ) || exit( 'Restricted access' );
 SPLoader::loadClass( 'opt.fields.info' );
+
 /**
  * @author Radek Suski
  * @version 1.0
@@ -28,13 +26,13 @@ class SPField_InfoAdm extends SPField_Info
 	public function save( &$attr )
 	{
 		$attr = $this->saveAttr( $attr );
-		parent::save($attr);
+		parent::save( $attr );
 	}
 
-	public function saveNew ( &$attr, $fid = 0 )
+	public function saveNew( &$attr, $fid = 0 )
 	{
 		$attr = $this->saveAttr( $attr );
-		if( $fid ) {
+		if ( $fid ) {
 			$this->fid = $fid;
 		}
 	}
@@ -46,12 +44,12 @@ class SPField_InfoAdm extends SPField_Info
 	 */
 	protected function saveAttr( &$attr )
 	{
-		if( $this->nid ) {
-			$this->nid = SPRequest::int('nid');
+		if ( $this->nid ) {
+			$this->nid = $attr[ 'nid' ];
 		}
-		if(!(isset($attr['viewInfo']))) {
-			$attr['viewInfo'] = $this->viewInfo;
-			$attr['entryInfo'] = $this->entryInfo;
+		if ( !( isset( $attr[ 'viewInfo' ] ) ) ) {
+			$attr[ 'viewInfo' ] = $this->viewInfo;
+			$attr[ 'entryInfo' ] = $this->entryInfo;
 		}
 		$data = array(
 				'key' => $this->nid . '-viewInfo',
