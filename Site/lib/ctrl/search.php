37be09b46cc4d2a51cd6ab7ed7588f022989092f
TrinitySigsiu <sigrid.suski@googlemail.com>
2015-11-29 12:19:59 +0100

    (!) Font Awesome 4 updated to version 4.5.0

--- Site/lib/ctrl/search.php
+++ Site/lib/ctrl/search.php
@@ -1,10 +1,12 @@
 <?php
 /**
  * @package: SobiPro Library
+ *
  * @author
  * Name: Sigrid Suski & Radek Suski, Sigsiu.NET GmbH
  * Email: sobi[at]sigsiu.net
  * Url: https://www.Sigsiu.NET
+ *
  * @copyright Copyright (C) 2006 - 2015 Sigsiu.NET GmbH (https://www.sigsiu.net). All rights reserved.
  * @license GNU/LGPL Version 3
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License version 3
