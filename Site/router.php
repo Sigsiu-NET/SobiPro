a35a678ac35df2aebfcdc30fa6b1118b5f074bdf
Radek Suski <suski.radek@gmail.com>
2016-06-29 10:02:46 +0200

    (#) Removing empty segment from router (Issue #1373)

--- Site/router.php
+++ Site/router.php
@@ -87,6 +87,9 @@ function SobiProBuildRoute( &$query )
 	if ( count( $segments ) && strstr( $segments[ count( $segments ) - 1 ], '.' ) ) {
 		$segments[ ] = '';
 	}
+	if ( $segments[ count( $segments ) - 1 ] == '' ) {
+		unset( $segments[ count( $segments ) - 1 ] );
+	}
 	return $segments;
 }
 
