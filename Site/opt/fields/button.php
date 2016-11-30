34e744a4c4e976af18ca0be31afc0a8aecba6e76
TrinitySigsiu <sigrid.suski@googlemail.com>
2016-11-30 17:40:59 +0100

    (+) New field 'Button'

--- Site/opt/fields/button.php
+++ Site/opt/fields/button.php
@@ -37,219 +37,18 @@ class SPField_Button extends SPField_Url implements SPFieldInterface
 	protected $cssClassView = 'spClassViewButton';
 	/** * @var string */
 	protected $cssClassEdit = 'spClassEditButton';
+	/** * @var string */
+	protected $cssButtonClass = 'btn btn-default';
+	/** * @var string */
+	protected $cssIconClass = 'icon icon-download';
 	/** @var bool */
-	protected $validateUrl = false;
+	protected $useIcon = true;
 	/** @var array */
 	protected $allowedProtocols = array( 'http', 'https', 'relative' );
-	/** @var bool */
-	protected $newWindow = false;
-	/** @var bool */
-	protected $noFollow = false;
 	/** @var string */
 	protected $dType = 'special';
 	/** @var bool */
-	protected $countClicks = false;
-	/** @var bool */
-	protected $deleteClicks = true;
-	/** @var bool */
-	protected $counterToLabel = false;
-	/** @var bool */
-	protected $labelAsPlaceholder = false;
-	/** @var bool */
 	static private $CAT_FIELD = true;
-
-	/**
-	 * Shows the field in the edit entry or add entry form
-	 *
-	 * @param bool $return return or display directly
-	 *
-	 * @return string
-	 */
-	public function field( $return = false )
-	{
-		if ( !( $this->enabled ) ) {
-			return false;
-		}
-		$field = null;
-		$fdata = Sobi::Reg( 'editcache' );
-		if ( $fdata && is_array( $fdata ) ) {
-			$raw = $this->fromCache( $fdata );
-		}
-		else {
-			$raw = $this->getRaw();
-			if ( !( is_array( $raw ) ) ) {
-				try {
-					$raw = SPConfig::unserialize( $raw );
-				}
-				catch ( SPException $x ) {
-					$raw = null;
-				}
-			}
-		}
-		if ( $this->ownLabel ) {
-			$fieldTitle = null;
-			$class      = $this->cssClass . 'Title';
-			if ( defined( 'SOBIPRO_ADM' ) ) {
-				if ( $this->bsWidth ) {
-					$width = SPHtml_Input::_translateWidth( $this->bsWidth );
-					$class .= ' ' . $width;
-				}
-			}
-			$params = array( 'id' => $this->nid, 'class' => $class );
-			if ( $this->labelMaxLength ) {
-				$params[ 'maxlength' ] = $this->labelMaxLength;
-			}
-			if ( $this->labelWidth ) {
-				$params[ 'style' ] = "width: {$this->labelWidth}px;";
-			}
-			if ( strlen( $this->labelsLabel ) ) {
-				$this->labelsLabel = SPLang::clean( $this->labelsLabel );
-				//$fieldTitle .= "<label for=\"{$this->nid}\" class=\"{$this->cssClass}Title\">{$this->labelsLabel}</label>\n";
-				$params[ 'placeholder' ] = $this->labelsLabel;
-			}
-			$fieldTitle .= SPHtml_Input::text( $this->nid, ( ( is_array( $raw ) && isset( $raw[ 'label' ] ) ) ? SPLang::clean( $raw[ 'label' ] ) : null ), $params );
-		}
-		$protocols = array();
-		if ( count( $this->allowedProtocols ) ) {
-			foreach ( $this->allowedProtocols as $protocol ) {
-				$protocols[ $protocol ] = $protocol . '://';
-			}
-		}
-		else {
-			$protocols = array( 'http' => 'http://', 'https' => 'https://' );
-		}
-		$params = array( 'id' => $this->nid . '_protocol', 'size' => 1, 'class' => $this->cssClass . 'Protocol' );
-
-		if ( Sobi::Cfg( 'template.bootstrap3-styles' ) ) {
-			$protofield = '<div class="input-group"><div class="input-group-btn">';
-		}
-		else {
-			$protofield = '<div class="input-prepend"><div class="btn-group">';
-		}
-		$protofield .= SPHtml_Input::select( $this->nid . '_protocol', $protocols, ( ( is_array( $raw ) && isset( $raw[ 'protocol' ] ) ) ? $raw[ 'protocol' ] : 'http' ), false, $params );
-		$protofield .= '</div>';
-
-		//$field .= '<span class="spFieldUrlProtocol">://</span>';
-		$class = $this->required ? $this->cssClass . ' required' : $this->cssClass;
-		if ( defined( 'SOBIPRO_ADM' ) ) {
-			if ( $this->bsWidth ) {
-				$width = SPHtml_Input::_translateWidth( $this->bsWidth );
-				$class .= ' ' . $width;
-			}
-		}
-
-		$params = array( 'id' => $this->nid . '_url', 'class' => $class );
-		if ( $this->maxLength ) {
-			$params[ 'maxlength' ] = $this->maxLength;
-		}
-
-		//for compatibility reason still there
-		if ( $this->width ) {
-			$params[ 'style' ] = "width: {$this->width}px;";
-		}
-
-		$label = Sobi::Txt( 'FD.URL_ADDRESS' );
-		if ( ( !$this->ownLabel ) && ( $this->labelAsPlaceholder ) ) { // the field label will be shown only if labelAsPlaceholder is true and no own label for the URL is selected
-			$label = $this->__get( 'name' );
-		}
-		$params[ 'placeholder' ] = $label;
-		$value                   = ( is_array( $raw ) && isset( $raw[ 'url' ] ) ) ? $raw[ 'url' ] : null;
-		if ( $value == null ) {
-			if ( $this->defaultValue ) {
-				$value = $this->defaultValue;
-			}
-		}
-
-		$field .= $protofield;
-		$field .= SPHtml_Input::text( $this->nid . '_url', $value, $params );
-		$field .= '</div>';
-
-		if ( $this->ownLabel ) {
-			$field = "\n<div class=\"spFieldUrlLabel\">{$fieldTitle}</div>\n<div class=\"spFieldUrl\">{$field}</div>";
-		}
-		else {
-			$field = "\n<div class=\"spFieldUrl\">{$field}</div>";
-		}
-
-		if ( $this->countClicks && $this->sid && ( $this->deleteClicks || SPFactory::user()->isAdmin() ) ) {
-			$counter = $this->getCounter();
-			if ( $counter ) {
-				SPFactory::header()->addJsFile( 'opt.field_url_edit' );
-			}
-			$classes = 'btn btn-default spCountableReset';
-			$attr    = array();
-			if ( !( $counter ) ) {
-				$attr[ 'disabled' ] = 'disabled';
-			}
-			$field .= SPHtml_Input::button( $this->nid . '_reset', Sobi::Txt( 'FM.URL.EDIT_CLICKS', $counter ), null, $classes );
-		}
-		if ( !$return ) {
-			echo $field;
-		}
-		else {
-			return $field;
-		}
-	}
-
-	public function compareRevisions( $revision, $current )
-	{
-		return array( 'current' => $this->paresUrl( $current ), 'revision' => $this->paresUrl( $revision ) );
-	}
-
-	protected function paresUrl( $url )
-	{
-		if ( $url[ 'protocol' ] == 'relative' ) {
-			$dUrl = $url[ 'url' ];
-		}
-		else {
-			$dUrl = $url[ 'protocol' ] . '://' . $url[ 'url' ];
-		}
-		if ( isset( $url[ 'label' ] ) ) {
-			return "{$url['label']}\n{$dUrl}";
-		}
-		else {
-			return $dUrl;
-		}
-	}
-
-
-	private	function fromCache( $cache )
-	{
-		$data = array();
-		if ( isset( $cache ) && isset( $cache[ $this->nid ] ) ) {
-			$data[ 'label' ] = $cache[ $this->nid ];
-		}
-		if ( isset( $cache ) && isset( $cache[ $this->nid . '_url' ] ) ) {
-			$data[ 'url' ] = $cache[ $this->nid . '_url' ];
-		}
-		if ( isset( $cache ) && isset( $cache[ $this->nid . '_protocol' ] ) ) {
-			$data[ 'protocol' ] = $cache[ $this->nid . '_protocol' ];
-		}
-
-		return $data;
-	}
-
-
-	/**
-	 * @param $real
-	 * @param $sid
-	 * @param $nid
-	 *
-	 * @return string
-	 */
-	protected
-	static function getHits( $real, $sid, $nid )
-	{
-		$query = array( 'sid' => $sid, 'fid' => $nid, 'section' => Sobi::Section() );
-		if ( $real ) {
-			$query[ 'humanity>' ] = Sobi::Cfg( 'field_url.humanity', 90 );
-		}
-		$counter = SPFactory::db()
-			->select( 'count(*)', 'spdb_field_url_clicks', $query )
-			->loadResult();
-
-		return $counter;
-	}
 
 	/**
 	 * Returns the parameter list
@@ -257,7 +56,7 @@ class SPField_Button extends SPField_Url implements SPFieldInterface
 	 */
 	protected function getAttr()
 	{
-		return array( 'ownLabel', 'labelWidth', 'labelMaxLength', 'labelsLabel', 'validateUrl', 'allowedProtocols', 'newWindow', 'maxLength', 'width', 'countClicks', 'counterToLabel', 'itemprop', 'cssClassView', 'cssClassEdit', 'noFollow', 'showEditLabel', 'labelAsPlaceholder', 'defaultValue', 'bsWidth', 'deleteClicks' );
+		return array( 'ownLabel', 'labelWidth', 'labelMaxLength', 'labelsLabel', 'validateUrl', 'allowedProtocols', 'newWindow', 'maxLength', 'width', 'countClicks', 'counterToLabel', 'itemprop', 'cssClassView', 'cssClassEdit', 'noFollow', 'showEditLabel', 'labelAsPlaceholder', 'defaultValue', 'bsWidth', 'deleteClicks', 'useIcon', 'cssIconClass', 'cssButtonClass' );
 	}
 
 	/**
@@ -275,11 +74,13 @@ class SPField_Button extends SPField_Url implements SPFieldInterface
 				$url = $data[ 'protocol' ] . '://' . $data[ 'url' ];
 			}
 			if ( !( isset( $data[ 'label' ] ) && strlen( $data[ 'label' ] ) ) ) {
-				$data[ 'label' ] = ( $this->labelsLabel == '' ) ? $url : $this->labelsLabel;
+				$data[ 'label' ] = ( $this->labelsLabel == '' && !$this->useIcon ) ? $url : $this->labelsLabel;
 			}
 			$this->cssClass = strlen( $this->cssClass ) ? $this->cssClass : 'spFieldsData';
+			$this->cssClass = $this->cssClass . ' ' . $this->cssButtonClass;
 			$this->cssClass = $this->cssClass . ' ' . $this->nid;
-			$attributes     = array( 'href' => $url, 'class' => $this->cssClass );
+
+			$attributes = array( 'href' => $url, 'class' => $this->cssClass );
 			if ( $this->countClicks ) {
 				SPFactory::header()->addJsFile( 'opt.field_url' );
 				$this->cssClass           = $this->cssClass . ' ctrl-visit-countable';
@@ -291,7 +92,7 @@ class SPField_Button extends SPField_Url implements SPFieldInterface
 				}
 				$attributes[ 'class' ] = $this->cssClass;
 				if ( $this->counterToLabel ) {
-					$data[ 'label' ] = Sobi::Txt( 'FM.URL.COUNTER_WITH_LABEL', array( 'label' => $data[ 'label' ], 'counter' => $counter ) );
+					$data[ 'label' ] = Sobi::Txt( 'FM.URL.COUNTER_WITH_LABEL2', array( 'label' => $data[ 'label' ], 'counter' => $counter ) );
 				}
 			}
 			$this->cleanCss();
@@ -302,10 +103,17 @@ class SPField_Button extends SPField_Url implements SPFieldInterface
 				if ( $this->noFollow ) {
 					$attributes[ 'rel' ] = 'nofollow';
 				}
-				$data = array(
+				if ( $this->useIcon ) {
+					$f[ 'i' ] = array(
+						'_complex'    => 1,
+						'_data'       => ' ',
+						'_attributes' => array( 'class' => $this->cssIconClass )
+					);
+				}
+				$f[ 'span' ] = SPLang::clean( $data[ 'label' ] );
+				$data        = array(
 					'_complex'    => 1,
-					'_data'       => SPLang::clean( $data[ 'label' ] ),
-//						'_data' => SPLang::clean( $this->ownLabel ? $data[ 'label' ] : $data[ 'url' ] ),
+					'_data'       => $f,
 					'_attributes' => $attributes
 				);
 
@@ -315,289 +123,6 @@ class SPField_Button extends SPField_Url implements SPFieldInterface
 					'_attributes' => array( 'lang' => Sobi::Lang( false ), 'class' => $this->cssClass, 'counter' => $counter )
 				);
 			}
-		}
-	}
-
-	protected function getCounter( $real = true )
-	{
-		$counter = self::getHits( $real, $this->sid, $this->nid );
-
-		return $counter;
-	}
-
-	public function cleanData( $html )
-	{
-		$data = SPConfig::unserialize( $this->getRaw() );
-		$url  = null;
-		if ( isset( $data[ 'url' ] ) && strlen( $data[ 'url' ] ) ) {
-			if ( $data[ 'protocol' ] == 'relative' ) {
-				$url = Sobi::Cfg( 'live_site' ) . $data[ 'url' ];
-			}
-			else {
-				$url = $data[ 'protocol' ] . '://' . $data[ 'url' ];
-			}
-		}
-
-		return $url;
-	}
-
-	/**
-	 * Gets the data for a field, verify it and pre-save it.
-	 *
-	 * @param SPEntry $entry
-	 * @param string $tsId
-	 * @param string $request
-	 *
-	 * @return array
-	 */
-	public function submit( &$entry, $tsId = null, $request = 'POST' )
-	{
-		if ( count( $this->verify( $entry, SPFactory::db(), $request ) ) ) {
-			return SPRequest::search( $this->nid, $request );
-		}
-		else {
-			return array();
-		}
-	}
-
-
-	/**
-	 * @param SPEntry $entry
-	 * @param string $request
-	 *
-	 * @return string
-	 */
-	public function validate( $entry, $request )
-	{
-		return $this->verify( $entry, SPFactory::db(), $request );
-	}
-
-	/**
-	 * @param SPEntry $entry
-	 * @param SPdb $db
-	 * @param string $request
-	 *
-	 * @throws SPException
-	 * @return array
-	 */
-	private function verify( $entry, &$db, $request )
-	{
-		$save = array();
-		if ( $this->ownLabel ) {
-			$save[ 'label' ] = SPRequest::raw( $this->nid, null, $request );
-			/* check if there was a filter */
-			if ( $this->filter && strlen( $save[ 'label' ] ) ) {
-				$registry =& SPFactory::registry();
-				$registry->loadDBSection( 'fields_filter' );
-				$filters = $registry->get( 'fields_filter' );
-				$filter  = isset( $filters[ $this->filter ] ) ? $filters[ $this->filter ] : null;
-				if ( !( count( $filter ) ) ) {
-					throw new SPException( SPLang::e( 'FIELD_FILTER_ERR', $this->filter ) );
-				}
-				else {
-					if ( !( preg_match( base64_decode( $filter[ 'params' ] ), $save[ 'label' ] ) ) ) {
-						throw new SPException( str_replace( '$field', $this->name, SPLang::e( $filter[ 'description' ] ) ) );
-					}
-				}
-			}
-		}
-		$data               = SPRequest::raw( $this->nid . '_url', null, $request );
-		$save[ 'protocol' ] = $db->escape( SPRequest::word( $this->nid . '_protocol', null, $request ) );
-		$dexs               = strlen( $data );
-		$data               = $db->escape( $data );
-		$data               = preg_replace( '/([a-z]{1,5}\:\/\/)/i', null, $data );
-		$save[ 'url' ]      = $data;
-		/* check if it was required */
-		if ( $this->required && !( $dexs ) ) {
-			throw new SPException( SPLang::e( 'FIELD_REQUIRED_ERR', $this->name ) );
-		}
-		/* check if there was an adminField */
-		if ( $this->adminField && $dexs ) {
-			if ( !( Sobi:: Can( 'entry.adm_fields.edit' ) ) ) {
-				throw new SPException( SPLang::e( 'FIELD_NOT_AUTH', $this->name ) );
-			}
-		}
-		/* check if it was free */
-		if ( !( $this->isFree ) && $this->fee && $dexs ) {
-			SPFactory::payment()->add( $this->fee, $this->name, $entry->get( 'id' ), $this->fid );
-		}
-		/* check if it should contains unique data */
-		if ( $this->uniqueData && $dexs ) {
-			$matches = $this->searchData( $data, Sobi::Reg( 'current_section' ) );
-			if ( count( $matches ) ) {
-				throw new SPException( SPLang::e( 'FIELD_NOT_UNIQUE', $this->name ) );
-			}
-		}
-		/* check if it was editLimit */
-		if ( $this->editLimit == 0 && !( Sobi::Can( 'entry.adm_fields.edit' ) ) && $dexs ) {
-			throw new SPException( SPLang::e( 'FIELD_NOT_AUTH_EXP', $this->name ) );
-		}
-		/* check if it was editable */
-		if ( !( $this->editable ) && !( Sobi::Can( 'entry.adm_fields.edit' ) ) && $dexs && $entry->get( 'version' ) > 1 ) {
-			throw new SPException( SPLang::e( 'FIELD_NOT_AUTH_NOT_ED', $this->name ) );
-		}
-
-		/* check the response code */
-		if ( $dexs && $this->validateUrl ) {
-			$rclass   = SPLoader::loadClass( 'services.remote' );
-			$err      = 0;
-			$response = 0;
-			try {
-				$connection = new $rclass();
-				$connection->setOptions(
-					array(
-						'url'            => $save[ 'protocol' ] . '://' . $data,
-						'connecttimeout' => 10,
-						'header'         => false,
-						'returntransfer' => true
-					)
-				);
-				$connection->exec();
-				$response = $connection->info( 'response_code' );
-				$err      = $connection->error( false );
-				$errTxt   = $connection->error();
-				$connection->close();
-				if ( $err ) {
-					Sobi::Error( $this->name(), SPLang::e( 'FIELD_URL_CANNOT_VALIDATE', $errTxt ), SPC::WARNING, 0, __LINE__, __FILE__ );
-				}
-			}
-			catch ( SPException $x ) {
-				Sobi::Error( $this->name(), SPLang::e( 'FIELD_URL_CANNOT_VALIDATE', $x->getMessage() ), SPC::WARNING, 0, __LINE__, __FILE__ );
-			}
-			if ( $err || ( $response != 200 ) ) {
-				$response = $err ? $errTxt : $response;
-				Sobi::Error( $this->name(), SPLang::e( 'FIELD_URL_ERR', $save[ 'protocol' ] . '://' . $data, $response ), SPC::WARNING, 0, __LINE__, __FILE__ );
-				throw new SPException( SPLang::e( 'FIELD_URL_ERR', $save[ 'protocol' ] . '://' . $data, $response ) );
-			}
-		}
-		if ( !( $dexs ) ) {
-			$save = null;
-		}
-
-		return $save;
-	}
-
-	/**
-	 * Gets the data for a field and save it in the database
-	 *
-	 * @param SPEntry $entry
-	 * @param string $request
-	 *
-	 * @return bool
-	 */
-	public function saveData( &$entry, $request = 'POST' )
-	{
-		if ( !( $this->enabled ) ) {
-			return false;
-		}
-
-		/* @var SPdb $db */
-		$db   =& SPFactory::db();
-		$save = $this->verify( $entry, $db, $request );
-		$this->setRawData( $save );
-
-		$time = SPRequest::now();
-		$IP   = SPRequest::ip( 'REMOTE_ADDR', 0, 'SERVER' );
-		$uid  = Sobi::My( 'id' );
-
-		/* if we are here, we can save these data */
-		/* collect the needed params */
-		$params                  = array();
-		$params[ 'publishUp' ]   = $entry->get( 'publishUp' );
-		$params[ 'publishDown' ] = $entry->get( 'publishDown' );
-		$params[ 'fid' ]         = $this->fid;
-		$params[ 'sid' ]         = $entry->get( 'id' );
-		$params[ 'section' ]     = Sobi::Reg( 'current_section' );
-		$params[ 'lang' ]        = Sobi::Lang();
-		$params[ 'enabled' ]     = $entry->get( 'state' );
-		$params[ 'baseData' ]    = $db->escape( SPConfig::serialize( $save ) );
-		$params[ 'approved' ]    = $entry->get( 'approved' );
-		$params[ 'confirmed' ]   = $entry->get( 'confirmed' );
-		/* if it is the first version, it is new entry */
-		if ( $entry->get( 'version' ) == 1 ) {
-			$params[ 'createdTime' ] = $time;
-			$params[ 'createdBy' ]   = $uid;
-			$params[ 'createdIP' ]   = $IP;
-		}
-		$params[ 'updatedTime' ] = $time;
-		$params[ 'updatedBy' ]   = $uid;
-		$params[ 'updatedIP' ]   = $IP;
-		$params[ 'copy' ]        = !( $entry->get( 'approved' ) );
-		if ( Sobi::My( 'id' ) == $entry->get( 'owner' ) ) {
-			--$this->editLimit;
-		}
-		$params[ 'editLimit' ] = $this->editLimit;
-
-		/* save it */
-		try {
-			/* Notices:
-			 * If it was new entry - insert
-			 * If it was an edit and the field wasn't filled before - insert
-			 * If it was an edit and the field was filled before - update
-			 *     " ... " and changes are not autopublish it should be insert of the copy .... but
-			 * " ... " if a copy already exist it is update again
-			 * */
-			$db->insertUpdate( 'spdb_field_data', $params );
-		}
-		catch ( SPException $x ) {
-			Sobi::Error( __CLASS__, SPLang::e( 'CANNOT_SAVE_DATA', $x->getMessage() ), SPC::WARNING, 0, __LINE__, __FILE__ );
-		}
-
-		/* if it wasn't edited in the default language, we have to try to insert it also for def lang */
-		if ( Sobi::Lang() != Sobi::DefLang() ) {
-			$params[ 'lang' ] = Sobi::DefLang();
-			try {
-				$db->insert( 'spdb_field_data', $params, true, true );
-			}
-			catch ( SPException $x ) {
-				Sobi::Error( __CLASS__, SPLang::e( 'CANNOT_SAVE_DATA', $x->getMessage() ), SPC::WARNING, 0, __LINE__, __FILE__ );
-			}
-		}
-	}
-
-	public function ProxyReset()
-	{
-		$eid = SPRequest::int( 'eid' );
-		// let's allow it for admins only right now
-		// later we can extend it a bit
-//		$entry = SPFactory::Entry( $eid );
-		if ( Sobi::Can( 'entry.manage.any' ) ) {
-			SPFactory::db()
-				->delete( 'spdb_field_url_clicks', array( 'section' => Sobi::Section(), 'sid' => $eid, 'fid' => $this->nid ) );
-		}
-		echo 1;
-	}
-
-	public function ProxyHits()
-	{
-		SPFactory::mainframe()->cleanBuffer()->customHeader();
-		$r = ( int ) self::getHits( true, SPRequest::int( 'eid' ), $this->nid );
-		echo $r;
-		exit;
-	}
-
-	public function ProxyCount()
-	{
-		SPLoader::loadClass( 'env.browser' );
-		SPLoader::loadClass( 'env.cookie' );
-		$browser   = SPBrowser::getInstance();
-		$this->nid = str_replace( array( '.count', '.' ), array( null, '_' ), SPRequest::task() );
-		$ident     = $this->nid . '_' . SPRequest::int( 'eid' );
-		$check     = SPRequest::cmd( 'count_' . $ident, null, 'cookie' );
-		if ( !( $check ) ) {
-			$data = array(
-				'date'        => 'FUNCTION:NOW()',
-				'uid'         => Sobi::My( 'id' ),
-				'sid'         => SPRequest::int( 'eid' ),
-				'fid'         => $this->nid,
-				'ip'          => SPRequest::ip( 'REMOTE_ADDR', 0, 'SERVER' ),
-				'section'     => Sobi::Section(),
-				'browserData' => $browser->get( 'browser' ),
-				'osData'      => $browser->get( 'system' ),
-				'humanity'    => $browser->get( 'humanity' )
-			);
-			SPCookie::set( 'count_' . $ident, 1, SPCookie::hours( 2 ) );
-			SPFactory::db()->insert( 'spdb_field_url_clicks', $data );
 		}
 	}
 }
