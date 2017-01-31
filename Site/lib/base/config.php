c312bc34e510c12c9bf3b6a992ef552d284b9873
TrinitySigsiu <sigrid.suski@googlemail.com>
2017-01-31 16:26:42 +0100

    (#) icons.json not read from current template

--- Site/lib/base/config.php
+++ Site/lib/base/config.php
@@ -1,11 +1,13 @@
 <?php
 /**
  * @package: SobiPro Library
+ *
  * @author
  * Name: Sigrid Suski & Radek Suski, Sigsiu.NET GmbH
  * Email: sobi[at]sigsiu.net
  * Url: https://www.Sigsiu.NET
- * @copyright Copyright (C) 2006 - 2015 Sigsiu.NET GmbH (https://www.sigsiu.net). All rights reserved.
+ *
+ * @copyright Copyright (C) 2006 - 2017 Sigsiu.NET GmbH (https://www.sigsiu.net). All rights reserved.
  * @license GNU/LGPL Version 3
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License version 3
  * as published by the Free Software Foundation, and under the additional terms according section 7 of GPL v3.
@@ -779,8 +781,8 @@ class SPConfig
 	protected function initIcons()
 	{
 		if ( !( count( $this->_icons ) ) ) {
-			if ( Sobi::Reg( 'current_template' ) && SPFs::exists( Sobi::Reg( 'current_template' ) . '/js/icons.json' ) ) {
-				$this->_icons = json_decode( Sobi::Reg( 'current_template' ) . '/js/icons.json', true );
+			if ( Sobi::Reg( 'current_template' ) && SPFs::exists( Sobi::Reg( 'current_template' ) . '/js/icons.json') ) {
+				$this->_icons = json_decode( SPFs::read(Sobi::FixPath(Sobi::Reg( 'current_template' ) . '/js/icons.json')), true );
 			}
 			else {
 				$this->_icons = json_decode( SPFs::read( SOBI_PATH . '/etc/icons.json' ), true );
