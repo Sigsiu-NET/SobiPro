e689f9cf7b045562314bfdee53fc4a908681166f
TrinitySigsiu <sigrid.suski@googlemail.com>
2018-01-12 11:12:54 +0100

    notice fixed (hope the right way)

--- Site/lib/sobi.php
+++ Site/lib/sobi.php
@@ -7,14 +7,14 @@
  * Email: sobi[at]sigsiu.net
  * Url: https://www.Sigsiu.NET
  *
- * @copyright Copyright (C) 2006 - 2017 Sigsiu.NET GmbH (https://www.sigsiu.net). All rights reserved.
+ * @copyright Copyright (C) 2006 - 2018 Sigsiu.NET GmbH (https://www.sigsiu.net). All rights reserved.
  * @license GNU/LGPL Version 3
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License version 3
  * as published by the Free Software Foundation, and under the additional terms according section 7 of GPL v3.
  * See http://www.gnu.org/licenses/lgpl.html and https://www.sigsiu.net/licenses.
  *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
  */
 
 use Sobi\Framework;
@@ -42,6 +42,7 @@ abstract class Sobi
 	 * @param bool $sef
 	 * @param bool $live
 	 * @param bool $forceItemId
+	 *
 	 * @return string
 	 */
 	public static function Url( $var = null, $js = false, $sef = true, $live = false, $forceItemId = false )
@@ -57,6 +58,7 @@ abstract class Sobi
 	 * @param int $line - file line
 	 * @param string $file - file name
 	 * @param null $sMsg - additional message
+	 *
 	 * @return null
 	 */
 	public static function Error( $section, $msg, $type = SPC::NOTICE, $code = 0, $line = null, $file = null, $sMsg = null )
@@ -95,12 +97,12 @@ abstract class Sobi
 				$sMsg .= ' [ ' . SPRequest::task() . ' ]';
 			}
 			$error = [
-					'section' => $section,
-					'message' => $msg,
-					'code' => $code,
-					'file' => $file,
-					'line' => $line,
-					'content' => $sMsg
+				'section' => $section,
+				'message' => $msg,
+				'code'    => $code,
+				'file'    => $file,
+				'line'    => $line,
+				'content' => $sMsg
 			];
 			trigger_error( 'json://' . json_encode( $error ), $type );
 		}
@@ -109,6 +111,7 @@ abstract class Sobi
 			SPLoader::loadClass( 'cms.base.mainframe' );
 			SPFactory::mainframe()->runAway( $msg, $code, SPConfig::getBacktrace() );
 		}
+
 		return null;
 	}
 
@@ -119,15 +122,18 @@ abstract class Sobi
 	public static function ReturnPoint()
 	{
 		if ( !isset( $_POST[ 'option' ] ) || $_POST[ 'option' ] != 'com_sobipro'
-				// wtf joobi??!!!!
-				|| ( isset( $_GET[ 'hidemainmenu' ] ) && isset( $_POST[ 'hidemainmenu' ] ) )
+			// wtf joobi??!!!!
+			|| ( isset( $_GET[ 'hidemainmenu' ] ) && isset( $_POST[ 'hidemainmenu' ] ) )
 		) {
 			Sobi::SetUserState( 'back_url', Sobi::Url( 'current' ) );
 		}
 		else {
 			$current = 'index.php?';
 			foreach ( $_POST as $k => $v ) {
-				$current .= $k . '=' . ( ( string )$v ) . '&amp;';
+				if ( is_array( $v ) ) {
+					$v = implode( ',', $v );
+				}
+				$current .= $k . '=' . ( ( string ) $v ) . '&amp;';
 			}
 			Sobi::SetUserState( 'back_url', $current );
 		}
@@ -138,6 +144,7 @@ abstract class Sobi
 	 *
 	 * @param string $time - time or date
 	 * @param string $format - section and key in the config
+	 *
 	 * @return string
 	 */
 	public static function Date( $time = null, $format = 'date.db_format' )
@@ -194,6 +201,7 @@ abstract class Sobi
 	 * Cleaning string for the output
 	 *
 	 * @param string $txt
+	 *
 	 * @return string
 	 */
 	public static function Clean( $txt )
@@ -209,6 +217,7 @@ abstract class Sobi
 	 * @param bool $adm - if true, admin file will be loaded
 	 * @param string $defSection - default section to load, only if not parsing sections
 	 * @param string $lang - language folder. Default the currently selected language will be loaded
+	 *
 	 * @deprecated
 	 * @return bool
 	 */
@@ -219,9 +228,11 @@ abstract class Sobi
 
 	/**
 	 * Trigger plugin action: Sobi::Trigger( 'LoadField', 'Search', array( &$fields ) );
+	 *
 	 * @param string $action - action to trigger
 	 * @param string $subject - subject of this action: e.g. entry, category, search etc
 	 * @param array $params - parameters to pass to the plugin
+	 *
 	 * @return bool
 	 */
 	public static function Trigger( $action, $subject = null, $params = [] )
@@ -244,10 +255,12 @@ abstract class Sobi
 	 * Can be also used like this:
 	 *         Sobi::Can( 'subject.action.ownership' )
 	 *         Sobi::Can( 'entry.access.unpublished_own' )
+	 *
 	 * @param $subject
 	 * @param string $action - e.g. edit
 	 * @param string $ownership - e.g. xx_own, all or valid
 	 * @param int $section - section. If not given, the current section will be used
+	 *
 	 * @return bool - true if authorized
 	 */
 	public static function Can( $subject, $action = 'access', $ownership = 'valid', $section = null )
@@ -260,6 +273,7 @@ abstract class Sobi
 	 *
 	 * @param string $key - The path of the state.
 	 * @param string $value - The value of the variable.
+	 *
 	 * @return mixed The previous state, if one existed.
 	 */
 	public static function SetUserState( $key, $value )
@@ -274,6 +288,7 @@ abstract class Sobi
 	 * @param string $request - The name of the variable passed in a request.
 	 * @param string $default - The default value for the variable if not found. Optional.
 	 * @param string $type - Filter for the variable.
+	 *
 	 * @return mixed
 	 */
 	public static function GetUserState( $key, $request, $default = null, $type = 'none' )
@@ -283,8 +298,10 @@ abstract class Sobi
 
 	/**
 	 * Sets the value of a user data.
+	 *
 	 * @param    string $key - The path of the state.
 	 * @param    string $value - The value of the variable.
+	 *
 	 * @return    mixed    The previous state, if one existed.
 	 */
 	public static function SetUserData( $key, $value )
@@ -294,8 +311,10 @@ abstract class Sobi
 
 	/**
 	 * Gets the value of a user data stored in session
+	 *
 	 * @param    string $key - The key of the user state variable.
 	 * @param    string $default - The default value for the variable if not found. Optional.
+	 *
 	 * @return    mixed
 	 */
 	public static function GetUserData( $key, $default = null )
@@ -314,6 +333,7 @@ abstract class Sobi
 	 * @param string $action
 	 * @param string $subject
 	 * @param mixed $params
+	 *
 	 * @return bool
 	 */
 	public static function TriggerPlugin( $action, $subject = null, $params = [] )
@@ -328,6 +348,7 @@ abstract class Sobi
 	 * @param string $key - the config key
 	 * @param mixed $def - default value
 	 * @param string $section - config section (not the SobiPro section)
+	 *
 	 * @return string
 	 */
 	public static function Cfg( $key, $def = null, $section = 'general' )
@@ -346,6 +367,7 @@ abstract class Sobi
 	 *
 	 * @param string $key - stored key
 	 * @param mixed $def - default value
+	 *
 	 * @return mixed
 	 */
 	public static function Reg( $key, $def = null )
@@ -357,6 +379,7 @@ abstract class Sobi
 	 * Returns current section id
 	 *
 	 * @param bool $name
+	 *
 	 * @return int
 	 */
 	public static function Section( $name = false )
@@ -365,10 +388,11 @@ abstract class Sobi
 		if ( !( $name ) ) {
 			return SPFactory::registry()->get( 'current_section' );
 		}
-		elseif ( ( string )$name == 'nid' ) {
+		elseif ( ( string ) $name == 'nid' ) {
 			if ( !( $section ) ) {
 				$section = SPFactory::Section( SPFactory::registry()->get( 'current_section' ) );
 			}
+
 			return $section->get( 'nid' );
 		}
 		else {
@@ -380,6 +404,7 @@ abstract class Sobi
 	 * Returns currently used language
 	 *
 	 * @param $path
+	 *
 	 * @return string
 	 * @deprecated use \Sobi\FileSystem\FileSystem::FixPath
 	 */
@@ -390,10 +415,12 @@ abstract class Sobi
 
 	/**
 	 * Returns currently used language
+	 *
 	 * @param bool $storage - force lang for storage.
 	 * If the $_POST array contain "sp_language" index and the $storage param is set, this language will be returned.
 	 * In other cases it is recommended to call this function with $storage = false. However because this happen only while recieving data from POST ///
 	 * @param bool $allowEmpty
+	 *
 	 * @return string
 	 */
 	public static function Lang( $storage = true, $allowEmpty = false )
@@ -438,6 +465,7 @@ abstract class Sobi
 			}
 		}
 		$lang = strlen( $lang ) ? $lang : ( $allowEmpty ? self::DefLang() : self::Lang( false, true ) );
+
 		return $lang;
 	}
 
@@ -446,6 +474,7 @@ abstract class Sobi
 		if ( self::Cfg( 'lang.ignore_default', false ) ) {
 			return self::Lang( false );
 		}
+
 		return strlen( self::Cfg( 'lang.default_lang', null ) ) ? self::Cfg( 'lang.default_lang' ) : SOBI_DEFLANG;
 	}
 
@@ -454,6 +483,7 @@ abstract class Sobi
 	 * e.g Sobi::My( 'id' ); Sobi::My( 'name' );
 	 *
 	 * @param string $property
+	 *
 	 * @return mixed
 	 */
 	public static function My( $property )
@@ -462,14 +492,17 @@ abstract class Sobi
 		if ( !( $user ) ) {
 			$user =& SPFactory::user();
 		}
+
 		return $user->get( $property );
 	}
 
 	/**
 	 * Method to initialise SobiPro from outside
+	 *
 	 * @param int $sid - section id
 	 * @param null $root - root of Joomla!
 	 * @param null $lang - language
+	 *
 	 * @return null
 	 */
 	public static function Initialise( $sid = 0, $root = null, $lang = null )
@@ -485,9 +518,11 @@ abstract class Sobi
 
 	/**
 	 * @deprecated since 1.1 replaced by {@link #Initialise()}
+	 *
 	 * @param null $root - root of Joomla!
 	 * @param null $lang - language
 	 * @param int $sid - section id
+	 *
 	 * @return null
 	 * @throws Exception
 	 */
@@ -534,16 +569,17 @@ abstract class Sobi
 			if ( $sid ) {
 				$path = [];
 				$id = $sid;
-				$path[] = ( int )$id;
+				$path[] = ( int ) $id;
 				while ( $id > 0 ) {
 					try {
 						$id = SPFactory::db()
-								->select( 'pid', 'spdb_relations', [ 'id' => $id ] )
-								->loadResult();
+							->select( 'pid', 'spdb_relations', [ 'id' => $id ] )
+							->loadResult();
 						if ( $id ) {
-							$path[] = ( int )$id;
+							$path[] = ( int ) $id;
 						}
-					} catch ( SPException $x ) {
+					}
+					catch ( SPException $x ) {
 						Sobi::Error( 'ExtCoreCtrl', SPLang::e( 'DB_REPORTS_ERR', $x->getMessage() ), SPC::ERROR, 500, __LINE__, __FILE__ );
 					}
 				}
