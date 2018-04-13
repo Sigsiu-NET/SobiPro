c45a20c6996772a2c6663e7132b942b94a59b459
Radek Suski <radek.suski@sigsiu.net>
2018-04-13 15:02:51 +0200

    Category field multilanguage problem #74

--- Site/opt/fields/category.php
+++ Site/opt/fields/category.php
@@ -435,8 +435,8 @@ class SPField_Category extends SPFieldType implements SPFieldInterface
 
 	protected function loadCategories()
 	{
-		if ( ( !( $this->_cats ) || !( count( $this->_cats ) ) ) && SPLoader::path( 'etc.categories.' . Sobi::Lang() . '-' . Sobi::Section(), 'front', true, 'json' ) ) {
-			$this->_cats = json_decode( SPFs::read( SPLoader::path( 'etc.categories.' . Sobi::Lang() . '-' . Sobi::Section(), 'front', true, 'json' ) ), true );
+		if ( ( !( $this->_cats ) || !( count( $this->_cats ) ) ) && SPLoader::path( 'etc.categories.' . Sobi::Lang( false ) . '-' . Sobi::Section(), 'front', true, 'json' ) ) {
+			$this->_cats = json_decode( SPFs::read( SPLoader::path( 'etc.categories.' . Sobi::Lang( false ) . '-' . Sobi::Section(), 'front', true, 'json' ) ), true );
 		}
 		if ( !( $this->_cats ) || !( count( $this->_cats ) ) ) {
 			$this->_cats = SPFactory::cache()
@@ -448,7 +448,7 @@ class SPField_Category extends SPFieldType implements SPFieldInterface
 			}
 			$cache = json_encode( $this->_cats );
 			if ( !( defined( 'SOBIPRO_ADM' ) ) ) {
-				SPFs::write( SPLoader::path( 'etc.categories.' . Sobi::Lang() . '-' . Sobi::Section(), 'front', false, 'json' ), $cache );
+				SPFs::write( SPLoader::path( 'etc.categories.' . Sobi::Lang( false ) . '-' . Sobi::Section(), 'front', false, 'json' ), $cache );
 			}
 
 		}
